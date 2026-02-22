74d0f61c574d582d3066ad82261eb83db100fcb5
cagnaith <vthehc@yahoo.com>
2026-02-22 13:43:14 -0600

    feat; Halved cost of luxury speeders from 20k - 75k to 10k - 37.5k

--- MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_vehicles_luxury.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_vehicles_luxury.lua
@@ -6,11 +6,11 @@ rpg_shop_speeders = {
 			name = "Luxury Vehicles & Airspeeders",
 			minItems = 1,
 			items = {		
-				{template = "object/tangible/tcg/series4/vehicle_deed_geonosian_speeder.iff", cost = 20000, max = 10, chance = 100, overridePrice = true },
-				{template = "object/tangible/tcg/series5/vehicle_deed_xj6_air_speeder.iff", cost = 35000, max = 10, chance = 100, overridePrice = true },	
-				{template = "object/tangible/deed/vehicle_deed/landspeeder_av21_deed.iff", cost = 45000, max = 10, chance = 100, overridePrice = true },
-				{template = "object/tangible/tcg/series3/vehicle_deed_koro2_exodrive_airspeeder.iff", cost = 55000, max = 10, chance = 100, overridePrice = true },
-				{template = "object/tangible/tcg/series1/vehicle_deed_organa_speeder.iff", cost = 75000, max = 10, chance = 100, overridePrice = true },						
+				{template = "object/tangible/tcg/series4/vehicle_deed_geonosian_speeder.iff", cost = 10000, max = 10, chance = 100, overridePrice = true },
+				{template = "object/tangible/tcg/series5/vehicle_deed_xj6_air_speeder.iff", cost = 17500, max = 10, chance = 100, overridePrice = true },	
+				{template = "object/tangible/deed/vehicle_deed/landspeeder_av21_deed.iff", cost = 22500, max = 10, chance = 100, overridePrice = true },
+				{template = "object/tangible/tcg/series3/vehicle_deed_koro2_exodrive_airspeeder.iff", cost = 27500, max = 10, chance = 100, overridePrice = true },
+				{template = "object/tangible/tcg/series1/vehicle_deed_organa_speeder.iff", cost = 37500, max = 10, chance = 100, overridePrice = true },						
 			}
 		},
 		
