e066247c04185aabd225aaf7ffda127231fc1063
cagnaith <vthehc@yahoo.com>
2026-02-22 13:07:26 -0600

    feat; Update Ranged weapon shops to include all non-faction Common and Uncommon ranged weapons. Updated prices based on rarity.

--- MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_weapons_ranged.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_weapons_ranged.lua
@@ -6,44 +6,66 @@ shop_weapons_ranged = {
 			name = "Pistols",
 			minItems = 1,
 			items = {
-				{template = "object/weapon/roleplay/ranged/pistol/flare_gun.iff", cost = 100, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/pistol/ion_pistol.iff", cost = 250, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/pistol/d18_pistol.iff", cost = 350, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/pistol/dl44_blaster_pistol.iff", cost = 400, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/pistol/dl44_metal_blaster_pistol.iff", cost = 450, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/pistol/power_5_heavy_blaster_pistol.iff", cost = 900, max = 10, chance = 100 },
+				{template = "object/weapon/roleplay/ranged/pistol/antiriot_tangle_gun_7.iff", cost = 500, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/pistol/d18_pistol.iff", cost = 150, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/pistol/dl18_pistol.iff", cost = 150, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/pistol/dl44_blaster_pistol.iff", cost = 700, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/pistol/flare_gun.iff", cost = 600, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/pistol/ion_stunner_pistol.iff", cost = 600, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/pistol/kyd21_blaster_pistol.iff", cost = 500, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/pistol/model_q2_holdout_blaster_pistol.iff", cost = 700, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/pistol/power_5_heavy_blaster_pistol.iff", cost = 700, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/pistol/se14_blaster_pistol.iff", cost = 800, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/pistol/striker_projectile_pistol.iff", cost = 300, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/pistol/wookiee_pistol.iff", cost = 800, max = 10, chance = 100, overridePrice = true },
 			}
 		},
-		{
-			name = "Rifles",
-			minItems = 1,
-			items = {
-				{template = "object/weapon/roleplay/ranged/rifle/ion_rifle.iff", cost = 300, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/rifle/tusken_elite_rifle.iff", cost = 400, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/rifle/sg82b_stun_rifle.iff", cost = 700, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/rifle/dp23_rifle.iff", cost = 1800, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/rifle/laser_rifle.iff", cost = 2000, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/rifle/berserker_rifle.iff", cost = 2000, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/rifle/bowcaster.iff", cost = 2000, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/rifle/cm_sniper_rifle.iff", cost = 2000, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/rifle/czerka_adventurer.iff", cost = 2000, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/rifle/dawnsorrow_rifle.iff", cost = 2000, max = 10, chance = 100 },
-			}
-		},
+
 		{
 			name = "Carbines",
 			minItems = 1,
 			items = {
+				{template = "object/weapon/roleplay/ranged/carbine/alliance_needler_carbine.iff", cost = 150, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/carbine/bothan_bola_carbine.iff", cost = 150, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/carbine/dc15s_carbine.iff", cost = 200, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/carbine/e5_carbine.iff", cost = 200, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/carbine/wookiee_carbine.iff", cost = 800, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/carbine/laser_carbine.iff", cost = 300, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/carbine/elite_carbine.iff", cost = 700, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/carbine/advanced_laser_carbine.iff", cost = 800, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/carbine/corestrike_rancor_carbine.iff", cost = 600, max = 10, chance = 100, overridePrice = true },
+				},
+		},
 
-				{template = "object/weapon/roleplay/ranged/carbine/ee3_carbine.iff", cost = 600, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/carbine/laser_carbine.iff", cost = 700, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/carbine/elite_carbine.iff", cost = 1500, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/carbine/charric_carbine.iff", cost = 1500, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/carbine/corestrike_rancor_carbine.iff", cost = 1500, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/carbine/firestar_10k.iff", cost = 1500, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/carbine/proton_carbine.iff", cost = 1500, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/carbine/sfor_republic_carbine.iff", cost = 1500, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/ranged/carbine/wookiee_carbine.iff", cost = 1500, max = 10, chance = 100 },
+		{
+			name = "Rifles",
+			minItems = 1,
+			items = {
+				{template = "object/weapon/roleplay/ranged/rifle/a280_blaster_rifle.iff", cost = 800, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/rifle/bowcaster.iff", cost = 800, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/rifle/dlt19_heavy_blaster_rifle.iff", cost = 800, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/rifle/ion_rifle.iff", cost = 900, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/rifle/j1_rifle.iff", cost = 800, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/rifle/laser_rifle.iff", cost = 400, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/rifle/sg82b_stun_rifle.iff", cost = 400, max = 10, chance = 100, overridePrice = true },
+			}
+		},
+
+		{
+			name = "Sniper Rifles",
+			minItems = 1,
+			items = {
+				{template = "object/weapon/roleplay/ranged/rifle/dlt20a_heavy_blaster_rifle.iff", cost = 600, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/rifle/czerka_adventurer.iff", cost = 600, max = 10, chance = 100, overridePrice = true },
+			}
+		},
+
+		{
+			name = "Shotguns",
+			minItems = 1,
+			items = {
+				{template = "object/weapon/roleplay/ranged/rifle/dp23_rifle.iff", cost = 800, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/ranged/pistol/scatter_pistol.iff", cost = 800, max = 10, chance = 100, overridePrice = true },
 			}
 		},
 		
@@ -51,9 +73,8 @@ shop_weapons_ranged = {
 			name = "Grenades",
 			minItems = 1,
 			items = {
-				{template = "object/weapon/roleplay/thrown/grenade/fragmentation_grenade.iff", cost = 600, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/thrown/grenade/glop_grenade.iff", cost = 700, max = 10, chance = 100 },
-				{template = "object/weapon/roleplay/thrown/grenade/cryoban_grenade.iff", cost = 1500, max = 10, chance = 100 },
+				{template = "object/weapon/roleplay/thrown/grenade/fragmentation_grenade.iff", cost = 100, max = 10, chance = 100, overridePrice = true },
+				{template = "object/weapon/roleplay/thrown/grenade/glop_grenade.iff", cost = 100, max = 10, chance = 100, overridePrice = true },
 			}
 		},
 	}	
