2a0325bd37eb7bea369d249b972cc36f6dd8779a
cagnaith <vthehc@yahoo.com>
2026-02-22 11:27:20 -0600

    feat; Test vendor update. Adds Marauder B armor to vendor. change bone price.

--- MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_armorsmith_tier1.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_armorsmith_tier1.lua
@@ -6,15 +6,15 @@ rpg_shop = {
 			name = "Bone Armor",
 			minItems = 2,
 			items = {
-				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_bicep_l.iff", cost = 1, max = 10, chance = 100 },
-				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_bicep_r.iff", cost = 1, max = 10, chance = 100 },
-				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_boots.iff", cost = 1, max = 10, chance = 100 },
-				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_bracer_l.iff", cost = 1, max = 10, chance = 100 },
-				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_bracer_r.iff", cost = 1, max = 10, chance = 100 },
-				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_chest_plate.iff", cost = 1, max = 10, chance = 100 },
-				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_gloves.iff", cost = 1, max = 10, chance = 100 },
-				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_helmet.iff", cost = 1, max = 10, chance = 100 },
-				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_leggings.iff", cost = 1, max = 10, chance = 100 },
+				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_bicep_l.iff", cost = 1, max = 10, chance = 100, overridePrice = true  },
+				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_bicep_r.iff", cost = 1, max = 10, chance = 100, overridePrice = true   },
+				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_boots.iff", cost = 1, max = 10, chance = 100, overridePrice = true   },
+				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_bracer_l.iff", cost = 1, max = 10, chance = 100, overridePrice = true   },
+				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_bracer_r.iff", cost = 1, max = 10, chance = 100, overridePrice = true   },
+				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_chest_plate.iff", cost = 1, max = 10, chance = 100, overridePrice = true   },
+				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_gloves.iff", cost = 1, max = 10, chance = 100, overridePrice = true   },
+				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_helmet.iff", cost = 1, max = 10, chance = 100, overridePrice = true   },
+				{template = "object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_leggings.iff", cost = 1, max = 10, chance = 100, overridePrice = true   },
 			}
 		},
 		
@@ -29,7 +29,23 @@ rpg_shop = {
 				{template = "object/tangible/wearables/armor/roleplay/zam/armor_zam_wesell_pants.iff", cost = 1, max = 10, chance = 100 },
 			}
 		},
-		
+				
+		{
+			name = "Light Marauder Armor",
+			minItems = 2,
+			items = {
+				{template = "object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_bicep_l.iff", cost = 50, max = 10, chance = 100, overridePrice = true   },
+				{template = "object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_bicep_r.iff", cost = 50, max = 10, chance = 100, overridePrice = true   },
+				{template = "object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_boots.iff", cost = 50, max = 10, chance = 100, overridePrice = true   },
+				{template = "object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_bracer_l.iff", cost = 50, max = 10, chance = 100, overridePrice = true   },
+				{template = "object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_bracer_r.iff", cost = 50, max = 10, chance = 100, overridePrice = true   },
+				{template = "object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_chest_plate.iff", cost = 100, max = 10, chance = 100, overridePrice = true   },
+				{template = "object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_gloves.iff", cost = 50, max = 10, chance = 100, overridePrice = true   },
+				{template = "object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_helmet.iff", cost = 50, max = 10, chance = 100, overridePrice = true   },
+				{template = "object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_leggings.iff", cost = 50, max = 10, chance = 100, overridePrice = true   },
+			}
+		},
+
 		{
 			name = "Chitin Armor",
 			minItems = 2,
@@ -101,7 +117,7 @@ rpg_shop = {
 				{template = "object/tangible/wearables/armor/roleplay/kashyyykian_ceremonial/armor_kashyyykian_ceremonial_bicep_r.iff", cost = 1, max = 10, chance = 100 },
 				{template = "object/tangible/wearables/armor/roleplay/kashyyykian_ceremonial/armor_kashyyykian_ceremonial_bracer_l.iff", cost = 1, max = 10, chance = 100 },
 				{template = "object/tangible/wearables/armor/roleplay/kashyyykian_ceremonial/armor_kashyyykian_ceremonial_bracer_r.iff", cost = 1, max = 10, chance = 100 },
-				--{template = "object/tangible/wearables/armor/roleplay/kashyyykian_ceremonial/armor_kashyyykian_ceremonial_chest_plate.iff", cost = 1, max = 10, chance = 100 },
+				{template = "object/tangible/wearables/armor/roleplay/kashyyykian_ceremonial/armor_kashyyykian_ceremonial_chest_plate.iff", cost = 1, max = 10, chance = 100 },
 				{template = "object/tangible/wearables/armor/roleplay/kashyyykian_ceremonial/armor_kashyyykian_ceremonial_chestplate.iff", cost = 1, max = 10, chance = 100 },
 				{template = "object/tangible/wearables/armor/roleplay/kashyyykian_ceremonial/armor_kashyyykian_ceremonial_leggings.iff", cost = 1, max = 10, chance = 100 },
 			}
