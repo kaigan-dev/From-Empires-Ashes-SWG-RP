d52b37496b268848b9c64ae2d0e4d818eb034cda
cagnaith <vthehc@yahoo.com>
2026-02-22 13:26:17 -0600

    feat; Update stimpack prices.

--- MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_stimpack_novice.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_stimpack_novice.lua
@@ -6,11 +6,11 @@ rpg_shop_stimpack_novice = {
 			name = "Medicine",
 			minItems = 1,
 			items = {
-				{template = "object/tangible/medicine/roleplay/rp_stimpack_a.iff", cost = 75, max = 10, chance = 100, overridePrice = true },
-				{template = "object/tangible/medicine/roleplay/rp_stimpack_b.iff", cost = 125, max = 10, chance = 100, overridePrice = true },
-				{template = "object/tangible/medicine/roleplay/rp_stimpack_c.iff", cost = 250, max = 10, chance = 100, overridePrice = true },
+				{template = "object/tangible/medicine/roleplay/rp_stimpack_a.iff", cost = 150, max = 10, chance = 100, overridePrice = true },
+				{template = "object/tangible/medicine/roleplay/rp_stimpack_b.iff", cost = 250, max = 10, chance = 100, overridePrice = true },
+				{template = "object/tangible/medicine/roleplay/rp_stimpack_c.iff", cost = 400, max = 10, chance = 100, overridePrice = true },
 				{template = "object/tangible/medicine/roleplay/rp_stimpack_d.iff", cost = 500, max = 10, chance = 100, overridePrice = true },
-				{template = "object/tangible/medicine/roleplay/rp_stimpack_e.iff", cost = 1000, max = 10, chance = 100, overridePrice = true },
+				{template = "object/tangible/medicine/roleplay/rp_stimpack_e.iff", cost = 8000, max = 10, chance = 100, overridePrice = true },
 			}
 		},
 	}	
