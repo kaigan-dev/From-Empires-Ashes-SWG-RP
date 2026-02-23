61b71afe066a6bd831e7bfb07da3f498f787ccfa
cagnaith <vthehc@yahoo.com>
2026-02-22 18:17:18 -0600

    Feat; Removed duplicate Padded Chest armor from vendor list.

--- MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_armorsmith_tier1.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_armorsmith_tier1.lua
@@ -186,7 +186,7 @@ rpg_shop = {
 				{template = "object/tangible/wearables/armor/roleplay/padded/armor_padded_s01_gloves.iff", cost = 600, max = 10, chance = 100, overridePrice = true },
 				{template = "object/tangible/wearables/armor/roleplay/padded/armor_padded_s01_helmet.iff", cost = 600, max = 10, chance = 100, overridePrice = true },
 				{template = "object/tangible/wearables/armor/roleplay/padded/armor_padded_s01_leggings.iff", cost = 600, max = 10, chance = 100, overridePrice = true },
-				{template = "object/tangible/wearables/armor/roleplay/padded/armor_padded_s02_chest_plate.iff", cost = 1200, max = 10, chance = 100, overridePrice = true },
+				--{template = "object/tangible/wearables/armor/roleplay/padded/armor_padded_s02_chest_plate.iff", cost = 1200, max = 10, chance = 100, overridePrice = true },
 			}
 		},
 		
