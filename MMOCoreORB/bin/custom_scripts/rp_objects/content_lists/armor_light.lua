0ff0ca447465c0492bed33fce018fa5bc83e2a18
cagnaith <vthehc@yahoo.com>
2026-02-27 12:29:34 -0600

    feat: DM zone troubleshooting part 5. Definitely this time.

--- MMOCoreORB/bin/custom_scripts/rp_objects/content_lists/armor_light.lua
+++ MMOCoreORB/bin/custom_scripts/rp_objects/content_lists/armor_light.lua
@@ -9,16 +9,29 @@ content = {
 	{"object/tangible/wearables/armor/roleplay/ithorian_sentinel/ith_armor_s03_leggings.iff", },
 	{"object/tangible/wearables/armor/roleplay/ithorian_sentinel/ith_armor_s03_gloves.iff", },
 	{"object/tangible/wearables/armor/roleplay/ithorian_sentinel/ith_armor_s03_helmet.iff", },
-	
-	
-	
-	
+		
 	
 	{"object/tangible/wearables/armor/roleplay/kashyyykian_ceremonial/armor_kashyyykian_ceremonial_bicep_l.iff", },
-
-
+	{"object/tangible/wearables/armor/roleplay/kashyyykian_ceremonial/armor_kashyyykian_ceremonial_bicep_r.iff", },
+	{"object/tangible/wearables/armor/roleplay/kashyyykian_ceremonial/armor_kashyyykian_ceremonial_bracer_l.iff", },
+	{"object/tangible/wearables/armor/roleplay/kashyyykian_ceremonial/armor_kashyyykian_ceremonial_bracer_r.iff", },
+	{"object/tangible/wearables/armor/roleplay/kashyyykian_ceremonial/armor_kashyyykian_ceremonial_leggings.iff", },
+	{"object/tangible/wearables/armor/roleplay/kashyyykian_ceremonial/armor_kashyyykian_ceremonial_chestplate.iff", },
 	
-	{"object/tangible/wearables/wookiee/shared_wke_gloves_s02.iff", },
+
+	{"object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_bicep_l.iff", },
+	{"object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_bicep_r.iff", },
+	{"object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_bracer_l.iff", },
+	{"object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_bracer_r.iff", },
+	{"object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_boots.iff", },
+	{"object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_chest_plate.iff", },
+	{"object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_gloves.iff", },
+	{"object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_leggings.iff", },
+	{"object/tangible/wearables/armor/roleplay/marauder_b/armor_marauder_s02_helmet.iff", },
+
+
+
+
 	{"object/tangible/wearables/wookiee/shared_wke_gloves_s03.iff", },
 	{"object/tangible/wearables/wookiee/shared_wke_gloves_s04.iff", },
 	{"object/tangible/wearables/wookiee/shared_wke_hat_s01.iff", },
