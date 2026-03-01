b1e687297234206bd84ce1c6b0d7913cc49aa361
cagnaith <vthehc@yahoo.com>
2026-03-01 14:37:53 -0600

    feat: Tutorial text still a work in progress.

--- MMOCoreORB/bin/scripts/screenplays/borrie/vendor_themes/rpg_vt_tutorial.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/vendor_themes/rpg_vt_tutorial.lua
@@ -2,10 +2,10 @@ local themeTag = "rpg_tutorial"
 
 rpg_tutorial = {
 	dialog = {
-		greeting = "Why travel in something that just gets the job done, when you can travel in style? In comfort? Perhaps in a fine high class airspeeder...?", --1
-		browse = "Only the finest Corellian leather is used in the seating of some of our vehicles. Extra fees may or may not apply, my friend...", --2
-		items = "You cannot beat the comfort given by just being in one of our finest vehicles...", --2
-		startShopping = "We can only guarantee the finest engineering. Some of the designs were lifted from luxury yachts in the Old Republic, you know.", --3
+		greeting = "Welcome to FEA! There are a few steps you'll want to take in order to create a character.", --1
+		browse = "The first thing to do is allocate your attributes. These serve as soft maximums for your skills, in addition to providing direct benefits such as increasing your health. If the window for training isn't already open, enter /train.", --2
+		items = "Next you should allocate skills. This is also done from the /train menu. I would not recommend raising any skills above their parent attribute right now, as it will cost significantly more experience.", --2
+		startShopping = "When you leave this room, step across the hall to speak to the vendors there. Many of their offers will be too expensive for your starting budget. Marauder armor is quite affordable if you want it, and some cheap weapons and stims are also available. Additionally, clothes are freely available upstairs.", --3
 		abort = "Goodbye.", --4
 	}
 }
