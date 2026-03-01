20910594f49420d08b67490bfeaf584312807551
cagnaith <vthehc@yahoo.com>
2026-03-01 15:15:33 -0600

    feat: Tutorial NPC can speak!

--- MMOCoreORB/bin/scripts/screenplays/borrie/vendor_themes/rpg_vt_tutorial.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/vendor_themes/rpg_vt_tutorial.lua
@@ -6,7 +6,11 @@ rpg_tutorial = {
 		browse = "The first thing to do is allocate your attributes. These serve as soft maximums for your skills, in addition to providing direct benefits such as increasing your health. If the window for training isn't already open, enter /train.", --2
 		items = "Next you should allocate skills. This is also done from the /train menu. I would not recommend raising any skills above their parent attribute right now, as it will cost significantly more experience.", --2
 		startShopping = "When you leave this room, step across the hall to speak to the vendors there. Many of their offers will be too expensive for your starting budget. Marauder armor is quite affordable if you want it, and some cheap weapons and stims are also available. Additionally, clothes are freely available upstairs.", --3
-		abort = "Goodbye.", --4
+		abort = "Farewell, traveller.", --4
+		firstthing = "The first thing to do is allocate your attributes. These serve as soft maximums for your skills, in addition to providing direct benefits such as increasing your health. If the window for training isn't already open, enter /train.",
+		secondthing = "Next you should allocate skills. This is also done from the /train menu. I would not recommend raising any skills above their parent attribute right now, as it will cost significantly more experience.",
+		thirdthing = "When you leave this room, step across the hall to speak to the vendors there. Many of their offers will be too expensive for your starting budget. Marauder armor is quite affordable if you want it, and some cheap weapons and stims are also available. Additionally, clothes are freely available upstairs.",
+		fourththing = "Finally, take a moment to rest and recover before stepping out into the world. Use the /rest command to do fill your health, action, and will pools.",
 	}
 }
  
