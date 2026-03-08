fb811fcc6b5fc14163ebe86be36f22b79dfbc937
kaigan-dev <adbolan@pm.me>
2026-03-08 18:49:33 -0500

    fix: Actually assigning the loop variable to the slot to check them in BorCombat.h.

--- MMOCoreORB/src/server/zone/borrie/BorCombat.h
+++ MMOCoreORB/src/server/zone/borrie/BorCombat.h
@@ -419,6 +419,7 @@ public:
 
                 // Prevent dodge in Heavy Armor
                 for (int i = 2; i <= 10; i++) { // Check each armor slot. Starting from 2 to make the loop slightly faster since body is checked at 1, 2, and 9.
+                    slot = i;
                     ManagedReference<ArmorObject*> armor = BorCharacter::GetArmorAtSlot(defender, GetSlotName(slot));
                     int rating = 0;
                     if (armor != nullptr && armor.get() != nullptr) {
