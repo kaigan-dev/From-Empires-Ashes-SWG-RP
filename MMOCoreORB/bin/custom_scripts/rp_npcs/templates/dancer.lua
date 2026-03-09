b86cc2f7bba03fc5764dbade76eb40e139839ecc
cagnaith <vthehc@yahoo.com>
2026-03-09 18:44:41 -0500

    feat: Weighted spawning test without adding a second delta factor.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/templates/dancer.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/dancer.lua
@@ -2,11 +2,9 @@ npc_template = {
 	randomName = "human",
 
     baseTemplates = {
-        "rp_human_female", 1,
-        "rp_twilek_female", 1,
+        "rp_human_female", 3,
+        "rp_twilek_female", 6,
         "rp_zabrak_female", 1,
-        "rp_trandoshan_female", 2,
-        "rp_bothan_female", 2,
     },
 
     skillTemplates = {"bear"},
@@ -15,15 +13,11 @@ npc_template = {
         	{"rp_human_female", {"xovros_hotel_receptionist"}},
             {"rp_twilek_female", {"xovros_hotel_receptionist"}},
             {"rp_zabrak_female", {"xovros_hotel_receptionist", "xovros_admin_gown"}},
-            {"rp_trandoshan_female", {"xovros_hotel_receptionist"}},
-            {"rp_bothan_female", {"xovros_hotel_receptionist"}},
     },
 
     customizationTemplates = {
        {"rp_human_female", {"random"}},
        {"rp_twilek_female", {"random"}},
        {"rp_zabrak_female", {"random"}},
-       {"rp_trandoshan_female", {"random"}},
-       {"rp_bothan_female", {"random"}},
     },
 }
