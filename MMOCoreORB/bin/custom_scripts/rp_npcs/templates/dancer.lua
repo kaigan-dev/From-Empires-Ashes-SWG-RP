5918bed70f67900a069193d64f07406e356e65b6
cagnaith <vthehc@yahoo.com>
2026-03-09 18:31:38 -0500

    feat: Nope. I just can't type.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/templates/dancer.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/dancer.lua
@@ -2,23 +2,22 @@ npc_template = {
 	randomName = "human",
 
     baseTemplates = {
-    --    "rp_human_female",
-        "rp_twilek female",
-    --    "rp_zabrak_female",
+        "rp_human_female",
+        "rp_twilek_female",
+        "rp_zabrak_female",
     },
 
     skillTemplates = {"bear"},
 
     equipmentTemplates = {
-      --  	{"rp_human_female", {"xovros_hotel_receptionist"}},
+        	{"rp_human_female", {"xovros_hotel_receptionist"}},
             {"rp_twilek_female", {"xovros_admin_gown"}},
-     --       {"rp_zabrak_female", {"xovros_hotel_receptionist"}},
+            {"rp_zabrak_female", {"xovros_hotel_receptionist"}},
     },
 
     customizationTemplates = {
-    --    {"rp_human_female", {"random"}},
-    --    {"rp_twilek_female", {"random"}},
-        {"rp_twilek_female", {"twilek_min"}},
-   --    {"rp_zabrak_female", {"random"}},
+       {"rp_human_female", {"random"}},
+       {"rp_twilek_female", {"random"}},
+       {"rp_zabrak_female", {"random"}},
     },
 }
