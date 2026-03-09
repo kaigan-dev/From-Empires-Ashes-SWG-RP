e72ee279d273b1666899f367243ae6f3972297a7
cagnaith <vthehc@yahoo.com>
2026-03-09 18:02:12 -0500

    feat: Template multi-species test.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/templates/dancer.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/dancer.lua
@@ -1,16 +1,26 @@
 npc_template = {
 	randomName = "human",
 
-    baseTemplates = {"rp_human_female"},
+    baseTemplates = {
+        "rp_human_female", 50,
+        "rp_twilek female", 100,
+        "rp_zabrak_female", 20,
+        "rp_rodian_female", 5,
+    },
 
     skillTemplates = {"bear"},
 
     equipmentTemplates = {
         	{"rp_human_female", {"xovros_hotel_receptionist"}},
+            {"rp_twilek_female", {"xovros_hotel_receptionist"}},
+            {"rp_zabrak_female", {"xovros_hotel_receptionist"}},
+            {"rp_rodian_female", {"xovros_hotel_receptionist"}},
     },
 
     customizationTemplates = {
         {"rp_human_female", {"random"}},
+        {"rp_twilek_female", {"random"}},
+        {"rp_zabrak_female", {"random"}},
+        {"rp_rodian_female", {"random"}},
     },
-
 }
