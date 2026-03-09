6d8591165129ac8e17b3d6600585cbc43d975be7
cagnaith <vthehc@yahoo.com>
2026-03-09 18:35:57 -0500

    feat: Template random clothes test.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/templates/dancer.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/dancer.lua
@@ -11,8 +11,9 @@ npc_template = {
 
     equipmentTemplates = {
         	{"rp_human_female", {"xovros_hotel_receptionist"}},
-            {"rp_twilek_female", {"xovros_admin_gown"}},
-            {"rp_zabrak_female", {"xovros_hotel_receptionist"}},
+            --{"rp_twilek_female", {"xovros_admin_gown"}},
+            {"rp_twilek_female", {"xovros_hotel_receptionist"}},
+            {"rp_zabrak_female", {"xovros_hotel_receptionist", "xovros_admin_gown", "civ/spacer/civilian_female_s07"}},
     },
 
     customizationTemplates = {
