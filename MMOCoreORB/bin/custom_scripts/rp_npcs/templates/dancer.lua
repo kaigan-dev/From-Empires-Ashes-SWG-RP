0923fad128d7b1f0618105de4ecbdf3d76735262
cagnaith <vthehc@yahoo.com>
2026-03-09 17:03:09 -0500

    feat: Test new template.

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/dancer.lua
@@ -0,0 +1,20 @@
+npc_template = {
+    randomname = "human",
+
+    baseTemplates = {"rp_human_female", "rp_twilek_female", "rp_zabrak_female", "rp_chiss_female", "rp_rodian_female"},
+
+    skillTemplates = {"zackaroth/creaturesmall"},
+
+    equipmentTemplates = {"xovros_hotel_receptionist"
+    },
+
+    customizationTemplates = {
+            {"rp_human_female", {"random"}},
+            {"rp_twilek_female", {"random"}}, 
+            {"rp_zabrak_female", {"random"}},
+            {"rp_chiss_female", {"random"}},
+            {"rp_rodian_female", {"random"}},
+
+    },
+
+}
