9ae1ae6ed85120f74b1886769a1db4cc88726c7f
cagnaith <vthehc@yahoo.com>
2026-03-09 18:20:12 -0500

    feat: Template possibly accounting for a requirement for lekku.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/templates/dancer.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/dancer.lua
@@ -12,7 +12,7 @@ npc_template = {
     equipmentTemplates = {
         	{"rp_human_female", {"xovros_hotel_receptionist"}},
             {"rp_twilek_female", {"xovros_hotel_receptionist"}},
-            {"rp_zabrak_female", {"xovros_hotel_receptionist"}},
+            {"rp_zabrak_female", {"xovros_admin_gown"}},
     },
 
     customizationTemplates = {
