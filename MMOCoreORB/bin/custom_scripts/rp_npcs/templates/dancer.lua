a34f79587996ff959458780287a3716e3cce3e17
cagnaith <vthehc@yahoo.com>
2026-03-09 18:41:26 -0500

    feat: Template with weighted spawning test.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/templates/dancer.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/dancer.lua
@@ -2,23 +2,28 @@ npc_template = {
 	randomName = "human",
 
     baseTemplates = {
-        "rp_human_female",
-        "rp_twilek_female",
-        "rp_zabrak_female",
+        "rp_human_female", 1,
+        "rp_twilek_female", 1,
+        "rp_zabrak_female", 1,
+        "rp_trandoshan_female", 2,
+        "rp_bothan_female", 2,
     },
 
     skillTemplates = {"bear"},
 
     equipmentTemplates = {
         	{"rp_human_female", {"xovros_hotel_receptionist"}},
-            --{"rp_twilek_female", {"xovros_admin_gown"}},
             {"rp_twilek_female", {"xovros_hotel_receptionist"}},
-            {"rp_zabrak_female", {"xovros_hotel_receptionist", "xovros_admin_gown", "civ/spacer/civilian_female_s07"}},
+            {"rp_zabrak_female", {"xovros_hotel_receptionist", "xovros_admin_gown"}},
+            {"rp_trandoshan_female", {"xovros_hotel_receptionist"}},
+            {"rp_bothan_female", {"xovros_hotel_receptionist"}},
     },
 
     customizationTemplates = {
        {"rp_human_female", {"random"}},
        {"rp_twilek_female", {"random"}},
        {"rp_zabrak_female", {"random"}},
+       {"rp_trandoshan_female", {"random"}},
+       {"rp_bothan_female", {"random"}},
     },
 }
