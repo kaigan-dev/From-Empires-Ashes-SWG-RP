05f0bb7947521421eeaec440a64e28c3ecd8aebe
cagnaith <vthehc@yahoo.com>
2026-03-10 14:45:16 -0500

    feat: Fix skills on all new templates.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/templates/bountyhunter.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/bountyhunter.lua
@@ -13,25 +13,11 @@ npc_template = {
 	},
 
 	skillTemplates = {
-		{"rp_human_male", {"bountyhunter"}},
-		{"rp_human_female", {"bountyhunter"}},
-		{"rp_bothan_male", {"bountyhunter"}},
-		{"rp_bothan_female", {"bountyhunter"}},
-		{"rp_chiss_male", {"bountyhunter"}},
-		{"rp_nautolan_male", {"bountyhunter"}},
-		{"rp_rodian_male", {"bountyhunter"}},
-		{"rp_rodian_female", {"bountyhunter"}},
-		{"rp_trandoshan_male", {"bountyhunter"}},
-		{"rp_trandoshan_female", {"bountyhunter"}},
-		{"rp_twilek_male", {"bountyhunter"}},
-		{"rp_twilek_female", {"bountyhunter"}},
-		{"rp_zabrak_male", {"bountyhunter"}},
-		{"rp_zabrak_female", {"bountyhunter"}},
+			"bountyhunter"
 	},
 	
 	
 	equipmentTemplates = {
-
 		{"rp_human_male", {"bountyhunter"}},
 		{"rp_human_female", {"bountyhunter"}},
 		{"rp_bothan_male", {"bountyhunter"}},
@@ -46,11 +32,9 @@ npc_template = {
 		{"rp_twilek_female", {"bountyhunter"}},
 		{"rp_zabrak_male", {"bountyhunter"}},
 		{"rp_zabrak_female", {"bountyhunter"}},
-
 	},	
 	
 	customizationTemplates = {
-
 		{"rp_human_male", {"random"}},
 		{"rp_human_female", {"random"}},
 		{"rp_bothan_male", {"random"}},
@@ -65,7 +49,6 @@ npc_template = {
 		{"rp_twilek_female", {"random"}},
 		{"rp_zabrak_male", {"random"}},
 		{"rp_zabrak_female", {"random"}},
-
 	},
 
 	--armor = "stormtrooper",
