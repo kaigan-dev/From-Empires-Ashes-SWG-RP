c49d6d532efe784fc0c055667adbcc1e96f4cf8f
cagnaith <vthehc@yahoo.com>
2026-03-10 15:24:26 -0500

    feat: Add ewoks. Test rancor armor just in case.

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/ewok.lua
@@ -0,0 +1,25 @@
+npc_template = {
+	randomName = "rodian",
+
+    baseTemplates = {
+        "ewok_f_greeter",
+        "ewok_guard",
+        "ewok_kelbe",
+        "ewok_m_greeter",
+        "ewok_protector",
+        "ewok_f_nonvendor_vendor",
+        "ewok_m_nonvendor_vendor"
+    },
+
+    skillTemplates = {
+        "civilian",
+    },
+
+    equipmentTemplates = {
+
+    },
+
+    customizationTemplates = {
+
+    },
+}
