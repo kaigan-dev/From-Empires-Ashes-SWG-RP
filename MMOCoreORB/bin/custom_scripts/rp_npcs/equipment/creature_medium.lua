18720e164ef6b10e1f9a1fbfe0547b5aa683424a
cagnaith <vthehc@yahoo.com>
2026-03-10 16:41:53 -0500

    feat: Another attempt at fixing the new creature damage equipment template.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/creature_medium.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/creature_medium.lua
@@ -1,3 +1,4 @@
 equipment = {
-	{"object/weapon/melee/unarmed/shared_unarmed_creature_medium.iff", },
+	--{"object/weapon/melee/unarmed/shared_unarmed_creature_medium.iff", },
+	{"object/weapon/melee/unarmed/unarmed_creature_medium.iff", },
 }
