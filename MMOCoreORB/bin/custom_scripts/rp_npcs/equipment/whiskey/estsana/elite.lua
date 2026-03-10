3c770d89889be68d26bbd5c08906b9038390fac1
cagnaith <vthehc@yahoo.com>
2026-03-10 13:58:57 -0500

    feat: BH template build attempt 1.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/whiskey/estsana/elite.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/whiskey/estsana/elite.lua
@@ -7,7 +7,7 @@ equipment = {
 	{"object/tangible/wearables/armor/roleplay/assault_trooper/armor_assault_trooper_bicep_r.iff", "/private/index_color_2", 10, "/private/index_color_1", 1, },
 	{"object/tangible/wearables/armor/roleplay/assault_trooper/armor_assault_trooper_bracer_r.iff", "/private/index_color_2", 10, "/private/index_color_1", 1, },
 	{"object/tangible/wearables/armor/roleplay/assault_trooper/armor_assault_trooper_bracer_l.iff", "/private/index_color_2", 2, "/private/index_color_1", 1, },
-	{"object/tangible/wearables/armor/roleplay/assault_trooper/armor_assault_trooper_utility_belt.iff", "/private/index_color_1", 1, },
+	{"object/tangible/wearables/armor/assault_trooper/armor_assault_trooper_utility_belt.iff", "/private/index_color_1", 1, },
 	{"object/tangible/wearables/armor/roleplay/assault_trooper/armor_assault_trooper_leggings.iff", "/private/index_color_2", 1, "/private/index_color_1", 1, },
 	{"object/tangible/wearables/armor/roleplay/assault_trooper/armor_assault_trooper_boots.iff", "/private/index_color_2", 2, "/private/index_color_1", 1, },
 	{"object/tangible/wearables/armor/roleplay/assault_trooper/armor_assault_trooper_gloves.iff", "/private/index_color_2", 2, "/private/index_color_1", 1, },
---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/bountyhunter.lua
@@ -0,0 +1,51 @@
+npc_template = {
+
+	--randomName = "human",
+	baseTemplates = {
+		"npc_human_male", "npc_human_female", "npc_human_male", "npc_human_female", "npc_human_male", "npc_human_male", 
+		"npc_bothan_male", "npc_bothan_female", 
+		"npc_chiss_male", 
+		"npc_nautolan_male", 
+		"npc_rodian_male", "npc_rodian_female", "npc_rodian_male", "npc_rodian_female", 
+		"npc_trandoshan_male", "npc_trandoshan_female", "npc_trandoshan_male", "npc_trandoshan_female", "npc_trandoshan_male", 
+		"npc_twilek_male", "npc_twilek_female", 
+		"npc_zabrak_male", "npc_zabrak_female", "npc_zabrak_male", "npc_zabrak_female", 
+	},
+	skillTemplates = {"bountyhunter"},
+	
+	equipmentTemplates = {
+		{"rp_human_male", {"bountyhunter"}},
+		{"rp_human_female", {"bountyhunter"}},
+		{"rp_bothan_male", {"bountyhunter"}},
+		{"rp_bothan_female", {"bountyhunter"}},
+		{"rp_chiss_male", {"bountyhunter"}},
+		{"rp_nautolan_male", {"bountyhunter"}},
+		{"rp_rodian_male", {"bountyhunter"}},
+		{"rp_rodian_female", {"bountyhunter"}},
+		{"rp_trandoshan_male", {"bountyhunter"}},
+		{"rp_trandoshan_female", {"bountyhunter"}},
+		{"rp_twilek_male", {"bountyhunter"}},
+		{"rp_twilek_female", {"bountyhunter"}},
+		{"rp_zabrak_male", {"bountyhunter"}},
+		{"rp_zabrak_female", {"bountyhunter"}},
+	},	
+	
+	customizationTemplates = {
+		{"rp_human_male", {"random"}},
+		{"rp_human_female", {"random"}},
+		{"rp_bothan_male", {"random"}},
+		{"rp_bothan_female", {"random"}},
+		{"rp_chiss_male", {"random"}},
+		{"rp_nautolan_male", {"random"}},
+		{"rp_rodian_male", {"random"}},
+		{"rp_rodian_female", {"random"}},
+		{"rp_trandoshan_male", {"random"}},
+		{"rp_trandoshan_female", {"random"}},
+		{"rp_twilek_male", {"random"}},
+		{"rp_twilek_female", {"random"}},
+		{"rp_zabrak_male", {"random"}},
+		{"rp_zabrak_female", {"random"}},
+	},
+
+	--armor = "stormtrooper",
+}
