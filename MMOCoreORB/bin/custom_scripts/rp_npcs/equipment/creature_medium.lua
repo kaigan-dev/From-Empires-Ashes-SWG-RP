d725db8deabef0fad2fff940dbaa5a38a6a3b55e
cagnaith <vthehc@yahoo.com>
2026-03-10 16:57:45 -0500

    feat: Fix duplication in creature_medium equipment list.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/creature_medium.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/creature_medium.lua
@@ -1,5 +1,4 @@
 equipment = {
 	{"object/weapon/melee/unarmed/shared_unarmed_creature_medium.iff", },
 	--{"object/weapon/melee/unarmed/unarmed_creature_medium.iff", },
-	{"object/weapon/melee/unarmed/shared_unarmed_creature_medium.iff", },
 }
