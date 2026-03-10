3ffc1c589b0dfa44cc9d79a506ef700b72089c07
cagnaith <vthehc@yahoo.com>
2026-03-10 08:54:54 -0500

    feat: Base NPC template test.

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/npc_human_male.lua
@@ -0,0 +1,19 @@
+npc_template = {
+	randomName = "human",
+
+    baseTemplates = {
+        "rp_human_male",
+    },
+
+    skillTemplates = {
+
+    },
+
+    equipmentTemplates = {
+
+    },
+
+    customizationTemplates = {
+       {"rp_human_male", {"random"}},
+    },
+}
