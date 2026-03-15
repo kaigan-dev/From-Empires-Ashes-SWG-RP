8c8ae7ee517c8e9f8e979aeace50495f40a3b957
cagnaith <vthehc@yahoo.com>
2026-03-15 17:50:01 -0500

    feat: Add lua for Endor Landing Pad

---
+++ MMOCoreORB/bin/scripts/object/building/endor/endor_landing_pad.lua
@@ -0,0 +1,5 @@
+object_building_endor_endor_landing_pad = object_building_endor_shared_endor_landing_pad:new {
+
+}
+
+ObjectTemplates:addTemplate(object_building_endor_endor_landing_pad, "object/building/endor/endor_landing_pad.iff")
