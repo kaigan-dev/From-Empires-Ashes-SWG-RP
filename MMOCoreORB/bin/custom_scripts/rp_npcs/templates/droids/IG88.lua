2690f60e549eefc293a89c9ae1da82132691ec61
cagnaith <vthehc@yahoo.com>
2026-03-18 21:37:43 -0500

    fix: Added comment in IG88 template. It really wants to be capitolized and won't take no for an answer.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/templates/droids/IG88.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/droids/IG88.lua
@@ -1,5 +1,6 @@
 npc_template = {
 	name = "An IG-88 Assassin Droid",
+    --The IG88 template really wants to be referred to using capitol letters for some reason. call it with "/dm createnpc droids/IG88".
 
     baseTemplates = {
         "ig_88",
