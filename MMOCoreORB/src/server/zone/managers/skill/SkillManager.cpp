de709acea3b495ec552ee487d746bc3f0d1ba6ba
cagnaith <vthehc@yahoo.com>
2026-03-20 11:53:08 -0500

    fix: Bracket problem in SkillManager.

--- MMOCoreORB/src/server/zone/managers/skill/SkillManager.cpp
+++ MMOCoreORB/src/server/zone/managers/skill/SkillManager.cpp
@@ -970,13 +970,13 @@ bool SkillManager::villageKnightPrereqsMet(CreatureObject* creature, const Strin
 
 	return fullTrees >= 2 && totalJediPoints >= 206;
 
-	
+}
 
-	static int getSkillCost(CreatureObject* player, String skillName){
-		//SkillManager* skillManager = SkillManager::instance();
-		Skill* skill = skillMap.get(skillName.hashCode());
+
+static int getSkillCost(CreatureObject* player, String skillName){
+	//SkillManager* skillManager = SkillManager::instance();
+	Skill* skill = skillMap.get(skillName.hashCode());
 		
-		player->sendSystemMessage("SkillManager getSkillCost: XP cost is " + std::to_string(skill->getXpCost()));
-		return skill->getXpCost();
-	}
+	player->sendSystemMessage("SkillManager getSkillCost: XP cost is " + std::to_string(skill->getXpCost()));
+	return skill->getXpCost();
 }
