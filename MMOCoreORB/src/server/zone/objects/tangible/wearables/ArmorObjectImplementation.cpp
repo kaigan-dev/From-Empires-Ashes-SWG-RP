aa4e9f650a28e6c6803a91c330efd27414ac2128
kaigan-dev <adbolan@pm.me>
2026-03-22 08:00:51 -0500

    fix: Revised null checks in ArmorObjectImplementation.cpp.

--- MMOCoreORB/src/server/zone/objects/tangible/wearables/ArmorObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/wearables/ArmorObjectImplementation.cpp
@@ -36,7 +36,7 @@ void ArmorObjectImplementation::loadTemplateData(SharedObjectTemplate* templateD
 		actionEncumbrance = armorTemplate->getActionEncumbrance();
 		mindEncumbrance = armorTemplate->getMindEncumbrance();
 	
-		if ((armorTemplate->getRating() && armorTemplate.get()->getRating() != nullptr)) {
+		if ((armorTemplate->getRating() != nullptr)) {
 			rating = armorTemplate->getRating();
 		}
 		else {
@@ -53,7 +53,7 @@ void ArmorObjectImplementation::loadTemplateData(SharedObjectTemplate* templateD
 		acid = armorTemplate->getAcid();
 		lightSaber = armorTemplate->getLightSaber();
 		
-		if ((armorTemplate->getRpSkillLevel() && armorTemplate.get()->getRpSkillLevel() != nullptr)) {
+		if ((armorTemplate->getRpSkillLevel() != nullptr)) {
 			rpSkillLevel = armorTemplate->getRpSkillLevel();
 		}
 		else {
