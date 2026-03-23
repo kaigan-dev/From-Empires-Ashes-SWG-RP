10814ad3fafb4ef9e04d38d57062fced120cae74
cagnaith <vthehc@yahoo.com>
2026-03-23 16:14:45 -0500

    feat: And one more for the Ithorians.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/random/rp_ithorian_female.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/random/rp_ithorian_female.lua
@@ -3,5 +3,7 @@ random_ranges = {
 	{"/shared_owner/blend_muscle", 0, 255},
 	{"/shared_owner/blend_fat", 0, 255},
 	{"/shared_owner/index_color_skin", 0, 255},
+	{"/private/index_color_2", 0, 31},
+	{"/private/index_color_3", 0, 31},
 	{"height", 0.88,1.15},
 }
--- MMOCoreORB/bin/custom_scripts/rp_npcs/random/rp_ithorian_male.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/random/rp_ithorian_male.lua
@@ -3,5 +3,7 @@ random_ranges = {
 	{"/shared_owner/blend_muscle", 0, 255},
 	{"/shared_owner/blend_fat", 0, 255},
 	{"/shared_owner/index_color_skin", 0, 255},
+	{"/private/index_color_2", 0, 255},
+	{"/private/index_color_3", 0, 255},
 	{"height", 0.88,1.15},
 }
