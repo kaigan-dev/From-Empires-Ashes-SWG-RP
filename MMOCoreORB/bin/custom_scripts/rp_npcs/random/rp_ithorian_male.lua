564801e7d02325a73bbfdd019b43fd28196dd4cf
cagnaith <vthehc@yahoo.com>
2026-03-23 16:07:55 -0500

    feat: Add height variation for Ithorians.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/random/rp_ithorian_male.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/random/rp_ithorian_male.lua
@@ -3,5 +3,5 @@ random_ranges = {
 	{"/shared_owner/blend_muscle", 0, 255},
 	{"/shared_owner/blend_fat", 0, 255},
 	{"/shared_owner/index_color_skin", 0, 255},
-	{"height", 1,1},
+	{"height", 0.88,1.15},
 }
