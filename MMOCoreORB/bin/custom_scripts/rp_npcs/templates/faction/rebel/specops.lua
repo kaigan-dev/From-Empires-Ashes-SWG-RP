c2dbb65248f9bd301196726e6804b497fdfc58bb
cagnaith <vthehc@yahoo.com>
2026-03-23 10:09:12 -0500

    fix: Nearing the end of template testing.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/templates/faction/rebel/specops.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/faction/rebel/specops.lua
@@ -2,18 +2,18 @@ npc_template = {
 
 	--randomName = "human",
 	baseTemplates = {
-		"dressed_assassin_mission_giver_reb_02",
+		--"dressed_assassin_mission_giver_reb_02",
 		"ep3_canopy_bandit_01",
 		"ep3_canopy_bandit_02",
 		"ep3_canopy_bandit_03",
-		"dressed_combatmedic_trainer_human_male_01",
-		"dressed_combatmedic_trainer_human_female_01",
+		--"dressed_combatmedic_trainer_human_male_01",
+		--"dressed_combatmedic_trainer_human_female_01",
 		"ep3_gursan_bryes",
-		"life_day_imperial_vendor",
-		"nym_themepark_cale_serasai",
+		--"life_day_imperial_vendor",
+		--"nym_themepark_cale_serasai",
 		"dressed_rebel_specforce_pathfinder_human_male_01",
-		"dressed_rebel_specforce_pathfinder_twk_female_01",
-		"dressed_rebel_specforce_pathfinder_twk_male_01",
+		--"dressed_rebel_specforce_pathfinder_twk_female_01",
+		--"dressed_rebel_specforce_pathfinder_twk_male_01",
 
 	},
 
