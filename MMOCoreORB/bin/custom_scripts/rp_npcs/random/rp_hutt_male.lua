408937968e0ed26f0c058f0e230edf2b0e79bfa5
cagnaith <vthehc@yahoo.com>
2026-03-23 15:13:30 -0500

    feat: Add customization to Hutts.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/random/rp_hutt_male.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/random/rp_hutt_male.lua
@@ -1,4 +1,7 @@
 random_ranges = {
 	{"/private/index_color_1", 0, 255},
+	{"/private/index_color_2", 0, 255},
+	{"/private/index_color_3", 0, 255},
+	{"/private/index_texture_1", 0, 25},
 	{"height", 1,1.2},
 }
