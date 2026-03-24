5c40bfcd4316a910ca33fc7f042451fbce3549ea
cagnaith <vthehc@yahoo.com>
2026-03-24 10:43:43 -0500

    feat: Add Dunes Devil freedom fighter template.

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/dunes_fighter.lua
@@ -0,0 +1,10 @@
+equipment = {
+{
+        "object/weapon/melee/unarmed/shared_unarmed_default_player.iff", },
+        {"object/weapon/melee/unarmed/unarmed_default_player.iff", "/private/index_color_blade", 0, },
+        {"object/tangible/wearables/jacket/jacket_s24.iff", "/private/index_color_1", 2, },
+        {"object/tangible/wearables/shirt/shirt_s24.iff", "/private/index_color_1", 66, },
+        {"object/tangible/wearables/pants/pants_s21.iff", "/private/index_color_2", 2, "/private/index_color_1", 67, },
+        {"object/tangible/wearables/boots/boots_s21.iff", "/private/index_color_2", 2, "/private/index_color_1", 38, },
+        {"object/weapon/roleplay/ranged/carbine/laser_carbine.iff", "/private/index_color_blade", 0, },
+}
---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/dunes_fighter.lua
@@ -0,0 +1,63 @@
+npc_template = {
+
+	randomName = "human",
+	title = "a Dunes Devil freedom fighter",
+	baseTemplates = {
+		"rp_human_male", "rp_human_female", "rp_human_male", "rp_human_female","rp_human_male", "rp_human_female",
+		"rp_bothan_male", "rp_bothan_female", 
+		"rp_chiss_male", "rp_chiss_female", 
+		"rp_rodian_male", "rp_rodian_female",
+		"rp_trandoshan_male", "rp_trandoshan_female",
+		"rp_twilek_male", "rp_twilek_female", "rp_twilek_male", "rp_twilek_female", 
+		"rp_zabrak_male", "rp_zabrak_female","rp_zabrak_male", "rp_zabrak_female",
+		"rp_gran_male",
+		"rp_wookiee_male", "rp_wookiee_female",
+	},
+
+	skillTemplates = {
+			"thug"
+	},
+	
+	
+	equipmentTemplates = {
+		{"rp_human_male", {"dunes_fighter"}},
+		{"rp_human_female", {"dunes_fighter"}},
+		{"rp_bothan_male", {"dunes_fighter"}},
+		{"rp_bothan_female", {"dunes_fighter"}},
+		{"rp_chiss_male", {"dunes_fighter"}},
+		{"rp_chiss_female", {"dunes_fighter"}},
+		{"rp_rodian_male", {"dunes_fighter"}},
+		{"rp_rodian_female", {"dunes_fighter"}},
+		{"rp_trandoshan_male", {"dunes_fighter"}},
+		{"rp_trandoshan_female", {"dunes_fighter"}},
+		{"rp_twilek_male", {"dunes_fighter"}},
+		{"rp_twilek_female", {"dunes_fighter"}},
+		{"rp_zabrak_male", {"dunes_fighter"}},
+		{"rp_zabrak_female", {"dunes_fighter"}},
+		{"rp_gran_male", {"dunes_fighter"}},
+		{"rp_wookiee_male", {"dunes_fighter"}},
+		{"rp_wookiee_female", {"dunes_fighter"}},
+	},	
+	
+	customizationTemplates = {
+		{"rp_human_male", {"random"}},
+		{"rp_human_female", {"random"}},
+		{"rp_bothan_male", {"random"}},
+		{"rp_bothan_female", {"random"}},
+		{"rp_chiss_male", {"random"}},
+		{"rp_chiss_female", {"random"}},
+		{"rp_rodian_male", {"random"}},
+		{"rp_rodian_female", {"random"}},
+		{"rp_trandoshan_male", {"random"}},
+		{"rp_trandoshan_female", {"random"}},
+		{"rp_twilek_male", {"random"}},
+		{"rp_twilek_female", {"random"}},
+		{"rp_zabrak_male", {"random"}},
+		{"rp_zabrak_female", {"random"}},
+		{"rp_gran_male", {"random"}},
+		{"rp_wookiee_male", {"random"}},
+		{"rp_wookiee_female", {"random"}},
+	},
+
+	--armor = "stormtrooper",
+}
