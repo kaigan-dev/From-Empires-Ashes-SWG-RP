009d6f2fdd697d32cf2b068915f1242eac344e59
cagnaith <vthehc@yahoo.com>
2026-03-24 12:40:22 -0500

    feat: Add Hutt templates. Add appropriate species variation to the Rebel templates.

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/faction/hutt/trooper_swamp.lua
@@ -0,0 +1,75 @@
+npc_template = {
+
+	randomName = "human",
+	title = "a Hutt thug",
+	baseTemplates = {
+		"rp_human_male", "rp_human_female", "rp_human_male", "rp_human_female","rp_human_male","rp_human_male",
+		"rp_rodian_male", "rp_rodian_female","rp_rodian_male", "rp_rodian_female",
+		"rp_trandoshan_male", "rp_trandoshan_female",
+		"rp_twilek_male", "rp_twilek_female",
+		"rp_zabrak_male", "rp_zabrak_female","rp_zabrak_male", "rp_zabrak_female",
+		"rp_gran_male",
+		"rp_gotal_male","rp_gotal_male",
+		"rp_nikto_male", "rp_nikto_male", "rp_nikto_male", "rp_nikto_male",
+		"rp_aqualish_male", "rp_aqualish_female",
+		"rp_chiss_male","rp_chiss_male",
+		"rp_devaronian_male",
+		"rp_weequay_male","rp_weequay_male","rp_weequay_male",
+		"rp_nautolan_male",
+	},
+
+	skillTemplates = {
+			"thug"
+	},
+	
+	
+	equipmentTemplates = {
+		{"rp_human_male", {"faction/hutt/trooper_swamp"}},
+		{"rp_human_female", {"faction/hutt/trooper_swamp"}},
+		{"rp_chiss_male", {"faction/hutt/trooper_swamp"}},
+		{"rp_chiss_female", {"faction/hutt/trooper_swamp"}},
+		{"rp_rodian_male", {"faction/hutt/trooper_swamp"}},
+		{"rp_rodian_female", {"faction/hutt/trooper_swamp"}},
+		{"rp_trandoshan_male", {"faction/hutt/trooper_swamp"}},
+		{"rp_trandoshan_female", {"faction/hutt/trooper_swamp"}},
+		{"rp_twilek_male", {"faction/hutt/trooper_swamp"}},
+		{"rp_twilek_female", {"faction/hutt/trooper_swamp"}},
+		{"rp_zabrak_male", {"faction/hutt/trooper_swamp"}},
+		{"rp_zabrak_female", {"faction/hutt/trooper_swamp"}},
+		{"rp_gran_male", {"faction/hutt/trooper_swamp"}},
+		{"rp_gotal_male", {"faction/hutt/trooper_swamp"}},
+		{"rp_nikto_male", {"faction/hutt/trooper_swamp"}},
+		{"rp_aqualish_male", {"faction/hutt/trooper_swamp"}},
+		{"rp_aqualish_female", {"faction/hutt/trooper_swamp"}},
+		{"rp_devaronian_male", {"faction/hutt/trooper_swamp"}},
+		{"rp_weequay_male", {"faction/hutt/trooper_swamp"}},
+		{"rp_nautolan_male", {"faction/hutt/trooper_swamp"}},
+		
+	},	
+	
+	customizationTemplates = {
+		{"rp_human_male", {"random"}},
+		{"rp_human_female", {"random"}},
+		{"rp_chiss_male", {"random"}},
+		{"rp_chiss_female", {"random"}},
+		{"rp_rodian_male", {"random"}},
+		{"rp_rodian_female", {"random"}},
+		{"rp_trandoshan_male", {"random"}},
+		{"rp_trandoshan_female", {"random"}},
+		{"rp_twilek_male", {"random"}},
+		{"rp_twilek_female", {"random"}},
+		{"rp_zabrak_male", {"random"}},
+		{"rp_zabrak_female", {"random"}},
+		{"rp_gran_male", {"random"}},
+		{"rp_gotal_male", {"random"}},
+		{"rp_nikto_male", {"random"}},
+		{"rp_aqualish_male", {"random"}},
+		{"rp_aquahlish_female", {"random"}},
+		{"rp_devaronian_male", {"random"}},
+		{"rp_weequay_male", {"random"}},
+		{"rp_nautolan_male", {"random"}},
+		
+	},
+
+	--armor = "stormtrooper",
+}
