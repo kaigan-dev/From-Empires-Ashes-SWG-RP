db9c87cb631994c6d751890f55150af49bef2311
cagnaith <vthehc@yahoo.com>
2026-03-27 14:40:29 -0500

    feat: Frankly ridiculous attempt to make hydrospanners delete themselves.

--- MMOCoreORB/bin/scripts/screenplays/borrie/rp_items/items/rp_item_hydrospanner.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/rp_items/items/rp_item_hydrospanner.lua
@@ -13,15 +13,21 @@ function BorRpItemHydrospannerMenuComponent:handleObjectMenuSelect(pObject, pPla
 	end
 	
 	if (selectedID == 20) then 
-		local skillMod = math.floor(CreatureObject(pPlayer):getSkillMod("rp_mechanics"))
-		local roll = math.floor(math.random(1,20))
-		local result = roll + skillMod	+ 2
+		local skillMod = math.floor(CreatureObject(pPlayer):getSkillMod("rp_mechanics"));
+		local roll = math.floor(math.random(1,20));
+		local result = roll + skillMod	+ 2;
 
-		local msg = CreatureObject(pPlayer):getFirstName().. " uses a hydrospanner, rolling Mechanics. \\#DBDBDB (1d20: " ..roll.. " + " ..skillMod.. " +2 (Item) = " ..result.. "\\#FFFFFF"
+		local msg = CreatureObject(pPlayer):getFirstName().. " uses a hydrospanner, rolling Mechanics. \\#DBDBDB (1d20: " ..roll.. " + " ..skillMod.. " +2 (Item) = " ..result.. "\\#FFFFFF";
 
-		broadcastMessageWithName(pPlayer, msg)
+		broadcastMessageWithName(pPlayer, msg);
 
-		pObject:decreaseUseCount()
+		pObject:decreaseUseCount();
+
+		TangibleObject* tano = cast<TangibleObject*>(pObject);
+		if(tano ~= nullptr)
+			tano:decreaseUseCount();
+			tano->decreaseUseCount();
+		end
 	end
 	
 	return 0
