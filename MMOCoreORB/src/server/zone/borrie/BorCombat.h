5827b94f616e104754348b2673896478144f0bcc
kaigan-dev <adbolan@pm.me>
2026-03-29 09:56:16 -0500

    debug: More logging fun in BorCombat.h.

--- MMOCoreORB/src/server/zone/borrie/BorCombat.h
+++ MMOCoreORB/src/server/zone/borrie/BorCombat.h
@@ -781,7 +781,8 @@ public:
         ManagedReference<ArmorObject*> armor = BorCharacter::GetArmorAtSlot(defender, GetSlotName(slot));
         int armorProtection = GetArmorProtection(defender, armor, GetDamageType(attackerWeapon));
         int armorSkillFlag = 0;
-        console.info(armor.toString());
+        String armorString = armor.toString(new String());
+        console.info(armorString);
         if (armor.get() != nullptr && (defender->getSkillMod("rp_strength") < armor->getRpSkillLevel())) {
             armorSkillFlag = 1;
         }
