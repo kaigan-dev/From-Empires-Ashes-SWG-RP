2e318d25f50d8a1b5381969cd2de0db53a79b03e
cagnaith <vthehc@yahoo.com>
2026-03-31 10:19:38 -0500

    feat: Attempt adding ewoks to spawngroup based on their object/mobile record. Re-add contents to the endor outfit group in case they're expected.

--- MMOCoreORB/bin/scripts/screenplays/borrie/civ_gen/spawn_groups/spawn_group_endor.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/civ_gen/spawn_groups/spawn_group_endor.lua
@@ -4,8 +4,11 @@ spawn_group = {
 	organic = {
 		--{"rp_human_male", 10},
 		--{"rp_human_female", 10},
-		{"rp_ewok_male", 10},
-		{"rp_ewok_female", 10},
+		--{"rp_ewok_male", 10},
+		--{"rp_ewok_female", 10},
+		{"dressed_ewok_f_01", 10},
+		{"dressed_ewok_m_01", 10},
+
 	},
 	droid = {
 		{"r5", 10},
