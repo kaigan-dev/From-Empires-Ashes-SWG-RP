80c62ff202fc046c7acebc1e5f57aef54dc0316b
cagnaith <vthehc@yahoo.com>
2026-03-31 10:27:56 -0500

    feat: Swapping back to the working ewok spawning.

--- MMOCoreORB/bin/scripts/screenplays/borrie/civ_gen/spawn_groups/spawn_group_endor.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/civ_gen/spawn_groups/spawn_group_endor.lua
@@ -4,10 +4,10 @@ spawn_group = {
 	organic = {
 		--{"rp_human_male", 10},
 		--{"rp_human_female", 10},
-		{"rp_ewok_male", 10},
-		{"rp_ewok_female", 10},
-		--{"dressed_ewok_f_01", 10},
-		--{"dressed_ewok_m_01", 10},
+		--{"rp_ewok_male", 10},  --These don't work.
+		--{"rp_ewok_female", 10},
+		{"dressed_ewok_f_01", 10},
+		{"dressed_ewok_m_01", 10},
 
 	},
 	droid = {
