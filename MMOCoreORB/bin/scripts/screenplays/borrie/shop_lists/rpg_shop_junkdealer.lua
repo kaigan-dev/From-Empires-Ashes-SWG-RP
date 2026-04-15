fa67795d9ac969b4b967ff52a21db149987f5625
cagnaith <vthehc@yahoo.com>
2026-04-15 09:56:26 -0500

    feat: Remove tripwire and non-functional droid caller from junk vendor shop list.

--- MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_junkdealer.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_junkdealer.lua
@@ -6,9 +6,9 @@ rpg_shop_general = {
 			name = "Utility Items",
 			minItems = 1,
 			items = {		
-				{template = "object/tangible/borrp/item/tripwire.iff", cost = 250, max = 10, chance = 100 },
+				--{template = "object/tangible/borrp/item/tripwire.iff", cost = 250, max = 10, chance = 100 },
 				{template = "object/tangible/borrp/item/fifty_foot_rope.iff", cost = 250, max = 10, chance = 100 },
-				{template = "object/tangible/borrp/item/droid_caller.iff", cost = 250, max = 10, chance = 100 },
+				--{template = "object/tangible/borrp/item/droid_caller.iff", cost = 250, max = 10, chance = 100 },
 				{template = "object/tangible/borrp/item/adhesive.iff", cost = 250, max = 10, chance = 100 },
 				{template = "object/tangible/borrp/item/restraining_bolt.iff", cost = 250, max = 10, chance = 100 },
 				{template = "object/tangible/borrp/item/sensor_pack.iff", cost = 250, max = 10, chance = 100 },
