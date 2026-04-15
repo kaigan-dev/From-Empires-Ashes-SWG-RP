92814f27246785dd7e645aad46ff42f259fdf0eb
cagnaith <vthehc@yahoo.com>
2026-04-15 07:46:22 -0500

    feat: Disable landing at the fueling station on Sulon, as it is not a major location.

--- MMOCoreORB/bin/scripts/screenplays/borrie/planet/planets/rp_sulon.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/planet/planets/rp_sulon.lua
@@ -10,7 +10,7 @@ planet = {
 	landing_points = {
 		--Tag, Name, Zone, x, z, y, angle, cell, shipVisible
 		{"saltalo_station_outskirts_a", "Saltalo Station A", "rp_sulon", 4478, 13.3, -5402, -90, 0, true},
-		{"akarlo_fueling_station", "Akarlo Fueling Station", "rp_sulon", 5367, 8, -5743, 0, 0, true},
+		--{"akarlo_fueling_station", "Akarlo Fueling Station", "rp_sulon", 5367, 8, -5743, 0, 0, true},
 	}	
 }
 
