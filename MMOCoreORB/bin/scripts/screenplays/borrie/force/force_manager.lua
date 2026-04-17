ec572f4198dae76a7f7a5c73a5b3dc6b8d6bb4e4
cagnaith <vthehc@yahoo.com>
2026-04-17 13:51:26 -0500

    fix: Test using stored getPlayerObject to provide a valid player to call getExperience against, as casting scene as Player isn't doing it.

--- MMOCoreORB/bin/scripts/screenplays/borrie/force/force_manager.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/force/force_manager.lua
@@ -1017,17 +1017,18 @@ function BorForce:dmTrainAlter(pPlayer, pSui, eventIndex, args)
 	CreatureObject(pPlayer):sendSystemMessage("Debug: Checking whether the target has force skill cap.")
 
 	--local capRemaining = PlayerObject(targetGhost):getExperience("rp_frc_skill_cap")
-	local capRemaining = PlayerObject(pTarget):getExperience("rp_frc_skill_cap")
+	--local capRemaining = PlayerObject(pTarget):getExperience("rp_frc_skill_cap")  -Blows up
+	local capRemaining = targetGhost:getExperience("rp_frc_skill_cap")
 	if (capRemaining <= 0) then
 		CreatureObject(pPlayer):sendSystemMessage("The target has already learned the maximum number of force skills that they can.")
 		return
 	else
 		CreatureObject(pPlayer):sendSystemMessage("Debug: The target has enough skill cap. Next we will check whether they have enough RP XP.")
-		local xpAmount = PlayerObject(pTarget):getExperience("rp_general")
+		local xpAmount = targetGhost:getExperience("rp_general")
 		if(xpAmount >= baseXpCost) then
 			local negativeCost = baseXpCost * -1
-			CreatureObject(pTarget):awardExperience("rp_general", negativeCost, false)
-			CreatureObject(pPlayer):sendSystemMessage("Debug: We have charged the target " .. baseXpCost .. " xp.")
+			--CreatureObject(pTarget):awardExperience("rp_general", negativeCost, false)
+			--CreatureObject(pPlayer):sendSystemMessage("Debug: We have charged the target " .. baseXpCost .. " xp.")
 		end
 		--BorSkill:CanTrainNextSkill()
 		--local xpAmount = targetGhost:getExperience("rp_general")
