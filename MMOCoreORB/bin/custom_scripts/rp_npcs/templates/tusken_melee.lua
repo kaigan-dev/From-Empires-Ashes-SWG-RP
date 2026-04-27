503a12ff1d0a1609d47d0659a84de90d172b395f
cagnaith <vthehc@yahoo.com>
2026-04-27 13:54:42 -0500

    feat: Add Tusken templates.

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/tusken_melee.lua
@@ -0,0 +1,19 @@
+npc_template = {
+	name = "A Tusken Raider",
+
+    baseTemplates = {
+        "rp_human_male",
+    },
+
+    skillTemplates = {
+        "thug",
+    },
+
+    equipmentTemplates = {
+            {"rp_human_male", {"tusken_melee"}},
+    },
+
+    customizationTemplates = {
+
+    },
+}
