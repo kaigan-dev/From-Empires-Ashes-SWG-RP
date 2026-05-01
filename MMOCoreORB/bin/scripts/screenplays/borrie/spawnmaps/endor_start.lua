679e264bbabd3d9be754bdc4bb0cc09e58395543
cagnaith <vthehc@yahoo.com>
2026-05-01 16:27:57 -0500

    feat: Terminal adjustment 5. Getting there.

--- MMOCoreORB/bin/scripts/screenplays/borrie/spawnmaps/endor_start.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/spawnmaps/endor_start.lua
@@ -138,11 +138,12 @@ function endor_start:spawnSceneObjects()
 	
 	--spawnSceneObject("rp_dathomir", "object/tangible/terminal/terminal_travel_rp.iff", 1520.12, 127, 1489.68, 0, 0.953717, 0, -0.300706, 0)
 
-	spawnSceneObject("rp_endor", "object/tangible/terminal/terminal_travel_rp.iff", 5965.8, 246.5, -2489.7, 0, 0.653717, 0, -0.300706, 0)
+	spawnSceneObject("rp_endor", "object/tangible/terminal/terminal_travel_rp.iff", 5965.8, 246.5, -2489.7, 0, 0.653717, 0, 0, 0)
+	spawnSceneObject("rp_endor", "object/tangible/terminal/terminal_travel_rp.iff", 5969.5, 247.5, -2490.9, 0, 0, 0, -0.300706, 0)
 	spawnSceneObject("rp_endor", "object/tangible/terminal/terminal_travel_rp.iff", 5960.7, 246.6, -2490.2, 0, 0.953717, 0, -0.300706, 0)
 	spawnSceneObject("rp_endor", "object/tangible/terminal/terminal_travel_rp.iff", 5957.8, 247, -2489.7, 0, 0.353717, 0, -0.300706, 0)
 	spawnSceneObject("rp_endor", "object/tangible/terminal/terminal_travel_rp.iff", 5954.7, 247, -2489.6, 0, 0.953717, 0, -0.600706, 0)
-	spawnSceneObject("rp_endor", "object/tangible/terminal/terminal_travel_rp.iff", 5954.7, 247, -2490, 0, 0.953717, 0, -0.900706, 0)
+	spawnSceneObject("rp_endor", "object/tangible/terminal/terminal_travel_rp.iff", 5950.7, 247, -2490, 0, 0.953717, 0, -0.900706, 0)
 
 end
 
