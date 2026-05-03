da6eb41323c21f64e12f4ea142a8fcdeb2bebc6f
Ashlynne187 <Perfectrogue@gmail.com>
2026-05-03 14:26:02 -0500

    feat: Detailed changes to dh17_pistol and files

--- MMOCoreORB/bin/scripts/object/weapon/roleplay/ranged/pistol/objects.lua
+++ MMOCoreORB/bin/scripts/object/weapon/roleplay/ranged/pistol/objects.lua
@@ -38,10 +38,10 @@ object_weapon_roleplay_ranged_pistol_de10_blaster_pistol = SharedWeaponObjectTem
 }
 ObjectTemplates:addClientTemplate(object_weapon_roleplay_ranged_pistol_de10_blaster_pistol, "object/weapon/roleplay/ranged/pistol/de10_blaster_pistol.iff")
 --************************************************************************************************************************************************************
- object_weapon_roleplay_ranged_pistol_dh17_pistol = SharedWeaponObjectTemplate:new {
+ object_weapon_roleplay_ranged_pistol_pistol_dh17 = SharedWeaponObjectTemplate:new {
 	clientTemplateFileName = "object/weapon/roleplay/ranged/pistol/pistol_dh17.iff"
 }
- ObjectTemplates:addClientTemplate(object_weapon_roleplay_ranged_pistol_dh17, "object/weapon/roleplay/ranged/pistol/pistol_dh17.iff")
+ ObjectTemplates:addClientTemplate(object_weapon_roleplay_ranged_pistol_pistol_dh17, "object/weapon/roleplay/ranged/pistol/pistol_dh17.iff")
 
 --************************************************************************************************************************************************************
 object_weapon_roleplay_ranged_pistol_dh18a_blaster_pistol = SharedWeaponObjectTemplate:new {
