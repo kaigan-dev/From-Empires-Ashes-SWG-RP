90e48221d2d96f61c8c5e6f1ca58a5fb51d39f51
cagnaith <vthehc@yahoo.com>
2026-05-09 11:41:01 -0500

    feat: Attempt to get armor rating loading properly on pre-existing items.

--- MMOCoreORB/src/server/zone/objects/tangible/wearables/ArmorObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/wearables/ArmorObjectImplementation.cpp
@@ -77,6 +77,7 @@ void ArmorObjectImplementation::notifyLoadFromDatabase() {
 	if (rating != LIGHT && templateObject->getClientTemplateFileName().contains("armor_bounty_hunter_"))
 		rating = LIGHT;
 */
+	rating = armorTemplate->getRating();
 }
 
 void ArmorObjectImplementation::fillAttributeList(AttributeListMessage* alm, CreatureObject* object) {
