3a6c70a012a18873c2120e366f919b507c19ac38
cagnaith <vthehc@yahoo.com>
2026-05-10 17:57:06 -0500

    feat: Add rock mite and small creature damage.

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/creatures/mite.lua
@@ -0,0 +1,21 @@
+npc_template = {
+
+	name = "A Rock Mite",
+	baseTemplates = {
+		"meatlump_tanc_mite",
+	},
+
+	skillTemplates = {
+			"creatures/small"
+	},
+	
+	
+	equipmentTemplates = {
+		{"meatlump_tanc_mite", {"creature_small"}},
+	},	
+	
+	customizationTemplates = {
+	},
+
+	--armor = "stormtrooper",
+}
