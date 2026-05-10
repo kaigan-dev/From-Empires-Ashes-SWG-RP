3fb732479942014ca28ccc237917fa9aa0ff3b20
cagnaith <vthehc@yahoo.com>
2026-05-10 18:14:48 -0500

    feat: Fix acklay base template.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/templates/creatures/acklay.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/creatures/acklay.lua
@@ -2,15 +2,15 @@ npc_template = {
 
 	name = "An Acklay",
 	baseTemplates = {
-		"acklay",
-		"acklay",
-		"acklay",
-		"acklay",
-		"acklay",
-		"acklay",
-		"acklay",
-		"acklay",
-		"acklay",
+		"acklay_hue",
+		"acklay_hue",
+		"acklay_hue",
+		"acklay_hue",
+		"acklay_hue",
+		"acklay_hue",
+		"acklay_hue",
+		"acklay_hue",
+		"acklay_hue",
 		"mutant_acklay",
 	},
 
@@ -20,7 +20,7 @@ npc_template = {
 	
 	
 	equipmentTemplates = {
-		{"acklay", {"creature_large"}},
+		{"acklay_hue", {"creature_large"}},
 		{"mutant_acklay", {"creature_large"}},
 	},	
 	
