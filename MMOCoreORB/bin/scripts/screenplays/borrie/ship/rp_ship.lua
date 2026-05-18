9db19dd2ed79423dbf2e152f677e1d091f84a176
cagnaith <vthehc@yahoo.com>
2026-05-18 15:28:22 -0500

    fix: Adjust usage of landing_points to call values from within the array, rather than the whole thing.

--- MMOCoreORB/bin/scripts/screenplays/borrie/ship/rp_ship.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/ship/rp_ship.lua
@@ -274,11 +274,11 @@ function BorRpShip:landShipCallback(pPlayer, pSui, eventIndex, rowIndex)
 
 	CreatureObject(pPlayer):sendSystemMessage("Attempting to land with parameters Ship: " .. tostring(pShip))
 	CreatureObject(pPlayer):sendSystemMessage("Attempting to land with parameters zone: " .. tostring(planetZone))
-	CreatureObject(pPlayer):sendSystemMessage("Attempting to land with parameters X: " .. tostring(planetObject.landing_points[3]))
-	CreatureObject(pPlayer):sendSystemMessage("Attempting to land with parameters Z: " .. tostring(planetObject.landing_points[4]))
-	CreatureObject(pPlayer):sendSystemMessage("Attempting to land with parameters Y: " .. tostring(planetObject.landing_points[5]))
+	CreatureObject(pPlayer):sendSystemMessage("Attempting to land with parameters X: " .. tostring(planetObject.landing_points[rowIndex + 1][3]))
+	CreatureObject(pPlayer):sendSystemMessage("Attempting to land with parameters Z: " .. tostring(planetObject.landing_points[rowIndex + 1][4]))
+	CreatureObject(pPlayer):sendSystemMessage("Attempting to land with parameters Y: " .. tostring(planetObject.landing_points[rowIndex + 1][5]))
 
-	BorRpShip:landShipAt(pShip, pPlayer, planetZone, planetObject.landing_points[3], planetObject.landing_points[4], planetObject.landing_points[5])
+	BorRpShip:landShipAt(pShip, pPlayer, planetZone, planetObject.landing_points[rowIndex + 1][3], planetObject.landing_points[rowIndex + 1][4], planetObject.landing_points[rowIndex + 1][5])
 
 	local message = shipName .. " has now landed at " .. selectedLandingSpot[2] .. "."
 	
