3b2f862cf8c5f5a32eed332f5f74f1118ec0cc53
cagnaith <vthehc@yahoo.com>
2026-05-18 15:31:43 -0500

    fix: Apparently our arrays are indexed at 1 instead of 0.

--- MMOCoreORB/bin/scripts/screenplays/borrie/ship/rp_ship.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/ship/rp_ship.lua
@@ -273,12 +273,12 @@ function BorRpShip:landShipCallback(pPlayer, pSui, eventIndex, rowIndex)
 	end
 
 	CreatureObject(pPlayer):sendSystemMessage("Attempting to land with parameters Ship: " .. tostring(pShip))
-	CreatureObject(pPlayer):sendSystemMessage("Attempting to land with parameters zone: " .. tostring(planetZone))
-	CreatureObject(pPlayer):sendSystemMessage("Attempting to land with parameters X: " .. tostring(planetObject.landing_points[rowIndex + 1][3]))
-	CreatureObject(pPlayer):sendSystemMessage("Attempting to land with parameters Z: " .. tostring(planetObject.landing_points[rowIndex + 1][4]))
-	CreatureObject(pPlayer):sendSystemMessage("Attempting to land with parameters Y: " .. tostring(planetObject.landing_points[rowIndex + 1][5]))
+	CreatureObject(pPlayer):sendSystemMessage("Attempting to land with parameters zone: " .. tostring(planetObject.landing_points[rowIndex + 1][3]))
+	CreatureObject(pPlayer):sendSystemMessage("Attempting to land with parameters X: " .. tostring(planetObject.landing_points[rowIndex + 1][4]))
+	CreatureObject(pPlayer):sendSystemMessage("Attempting to land with parameters Z: " .. tostring(planetObject.landing_points[rowIndex + 1][5]))
+	CreatureObject(pPlayer):sendSystemMessage("Attempting to land with parameters Y: " .. tostring(planetObject.landing_points[rowIndex + 1][6]))
 
-	BorRpShip:landShipAt(pShip, pPlayer, planetZone, planetObject.landing_points[rowIndex + 1][3], planetObject.landing_points[rowIndex + 1][4], planetObject.landing_points[rowIndex + 1][5])
+	BorRpShip:landShipAt(pShip, pPlayer, planetObject.landing_points[rowIndex + 1][3], planetObject.landing_points[rowIndex + 1][4], planetObject.landing_points[rowIndex + 1][5], planetObject.landing_points[rowIndex + 1][6])
 
 	local message = shipName .. " has now landed at " .. selectedLandingSpot[2] .. "."
 	
