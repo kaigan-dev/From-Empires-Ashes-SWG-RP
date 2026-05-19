1f005bb24a87aa7994c4a0898d9d241eba5b912c
cagnaith <vthehc@yahoo.com>
2026-05-18 19:39:29 -0500

    fix: Attempt 2 to populate data on ship based on control device.

--- MMOCoreORB/bin/scripts/screenplays/borrie/ship/shipMenus.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/ship/shipMenus.lua
@@ -79,14 +79,13 @@ function RpShipControlDeviceMenuComponent:fillObjectMenuResponse(pSceneObject, p
 	
 	local pShip = getShipFromControlDevice(pSceneObject)
 	
-	if(pShip ~= nil) then
-		SceneObject(pShip):setStoredString("flatteningTemplate", SceneObject(pSceneObject):getStoredString("flatteningTemplate"))
-		SceneObject(pShip):setStoredString("appearanceMobile", SceneObject(pSceneObject):getStoredString("appearanceMobile"))
-	end
-
 	if(pShip == nil) then
 		pShip = pSceneObject
 	end
+
+	CreatureObject(pPlayer):sendSystemMessage("Populating flatteningTemplate and appearanceMobile with values " .. tostring(SceneObject(pSceneObject):getStoredString("flatteningTemplate")) .. " and " .. tostring(SceneObject(pSceneObject):getStoredString("appearanceMobile")))
+	SceneObject(pShip):setStoredString("flatteningTemplate", SceneObject(pSceneObject):getStoredString("flatteningTemplate"))
+	SceneObject(pShip):setStoredString("appearanceMobile", SceneObject(pSceneObject):getStoredString("appearanceMobile"))
 	
 	local shipID = SceneObject(pShip):getObjectID()
 	
