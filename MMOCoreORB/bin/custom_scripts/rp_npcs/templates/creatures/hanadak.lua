b3d069c72682f7fcf043995d0bef8d837bae0f1a
cagnaith <vthehc@yahoo.com>
2026-05-22 17:43:56 -0500

    feat: Add template for Hanadak and fix one marauder armor piece that still had a negative resist.

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/creatures/hanadak.lua
@@ -0,0 +1,21 @@
+npc_template = {
+
+	name = "A Hanadak",
+	baseTemplates = {
+		"hanadak",
+	},
+
+	skillTemplates = {
+			"creatures/medium"
+	},
+	
+	
+	equipmentTemplates = {
+		{"hanadak", {"creature_medium"}},
+	},	
+	
+	customizationTemplates = {
+	},
+
+	--armor = "stormtrooper",
+}
