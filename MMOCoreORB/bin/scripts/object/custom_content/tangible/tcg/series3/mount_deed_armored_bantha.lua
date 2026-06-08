bfa7bbb8efe746afe317360d1a7a3aada3919e41
Ashlynne187 <Perfectrogue@gmail.com>
2026-06-08 08:53:22 -0500

    Feat: Comment out armored bantha deed for Prod

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/tcg/series3/mount_deed_armored_bantha.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/tcg/series3/mount_deed_armored_bantha.lua
@@ -1,7 +1,7 @@
 
 object_tangible_tcg_series3_mount_deed_armored_bantha = object_tangible_tcg_series3_shared_mount_deed_armored_bantha:new {
 
-	templateType = PETDEED,
+	/*templateType = PETDEED,
 	numberExperimentalProperties = {1, 1},
 	experimentalProperties = {"XX", "XX"},
 	experimentalWeights = {1, 1},
@@ -13,7 +13,7 @@ object_tangible_tcg_series3_mount_deed_armored_bantha = object_tangible_tcg_seri
 	experimentalCombineType = {0, 0},
 	generatedObjectTemplate = "mobile/pet/bantha_saddle_armored.iff",
 	controlDeviceObjectTemplate = "object/intangible/pet/bantha_hue.iff",
-	mobileTemplate = "bantha_saddle_armored",
+	mobileTemplate = "bantha_saddle_armored", */
 
 }
 ObjectTemplates:addTemplate(object_tangible_tcg_series3_mount_deed_armored_bantha, "object/tangible/tcg/series3/mount_deed_armored_bantha.iff")
