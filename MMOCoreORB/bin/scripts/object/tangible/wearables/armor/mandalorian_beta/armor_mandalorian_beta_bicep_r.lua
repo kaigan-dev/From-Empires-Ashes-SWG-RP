81b6d1f2fda46af27588daef926442710d47e059
cagnaith <vthehc@yahoo.com>
2026-06-14 05:39:00 -0500

    fix: Populate stats in mando beta gear so that it doesn't cause seg faults when accessed.

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/mandalorian_beta/armor_mandalorian_beta_bicep_r.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/mandalorian_beta/armor_mandalorian_beta_bicep_r.lua
@@ -1,4 +1,33 @@
 object_tangible_wearables_armor_mandalorian_beta_armor_mandalorian_beta_bicep_r = object_tangible_wearables_armor_mandalorian_beta_shared_armor_mandalorian_beta_bicep_r:new {
+templateType = ARMOROBJECT,
 
+
+	objectMenuComponent  = "ArmorObjectMenuComponent",
+
+
+	healthEncumbrance = 0,
+	armorEncumbrance = 0,
+	mindEncumbrance = 0,
+
+
+	maxCondition = 450,
+
+
+	rating = HEAVY,
+	rpskill = 6,
+
+
+	kinetic = 6,
+	energy = 8,
+	electricity = 0,
+	stun = 2,
+	blast = 0,
+	heat = 2,
+	cold = 2,
+	acid = 2,
+	lightsaber = 8,
+	lightSaber = 8,
+	
+	rarity = "Legendary",
 }
 ObjectTemplates:addTemplate(object_tangible_wearables_armor_mandalorian_beta_armor_mandalorian_beta_bicep_r, "object/tangible/wearables/armor/mandalorian_beta/armor_mandalorian_beta_bicep_r.iff")
