eab4339da67a85c55f0dcecab3d0e4d44ac39d63
canablyss <the.mac.atk@live.co.uk>
2026-06-15 16:44:23 +0100

    feat: listing fix

--- MMOCoreORB/bin/scripts/object/weapon/melee/plasma_saber/2h_sword/2h_sword_saber_aayla.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/plasma_saber/2h_sword/2h_sword_saber_aayla.lua
@@ -1,5 +1,5 @@
-object_weapon_roleplay_lightsaber_single_2h_2h_sword_saber_aayla = object_weapon_melee_plasma_saber_2h_sword_shared_2h_sword_saber_aayla:new {
-	alternateGrip = "object/weapon/roleplay/lightsaber/single/1h/sword_saber_aayla.iff",
+object_weapon_lightsaber_single_2h_2h_sword_saber_aayla = object_weapon_melee_plasma_saber_2h_sword_shared_2h_sword_saber_aayla:new {
+	alternateGrip = "object/weapon/lightsaber/single/1h/sword_saber_aayla.iff",
 
 	attackType = MELEEATTACK,
 	damageType = LIGHTSABER,
@@ -23,5 +23,5 @@ object_weapon_roleplay_lightsaber_single_2h_2h_sword_saber_aayla = object_weapon
 		{templateFile = "object/tangible/inventory/lightsaber_inventory_2.iff", x = 0, z = 0, y = 0, ox = 0, oy = 0, oz = 0, ow = 0, cellid = -1, containmentType = 4}
 	},
 }
-ObjectTemplates:addTemplate(object_weapon_roleplay_lightsaber_single_2h_2h_sword_saber_aayla, "object/weapon/roleplay/lightsaber/single/2h/2h_sword_saber_aayla.iff")
+ObjectTemplates:addTemplate(object_weapon_lightsaber_single_2h_2h_sword_saber_aayla, "object/weapon/lightsaber/single/2h/2h_sword_saber_aayla.iff")
 
