7e1beb3c9fa467a3918d2d61167eb0c76ba72a75
cagnaith <vthehc@yahoo.com>
2026-06-29 10:08:19 -0500

    feat: Add mando_rebel template for Telios.

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/mando_rebel.lua
@@ -0,0 +1,45 @@
+npc_template = {
+
+	randomName = "human",
+	baseTemplates = {
+		"rp_human_male", "rp_human_female", "rp_human_male", "rp_human_female", "rp_human_male", "rp_human_male",  "rp_human_male", "rp_human_male", "rp_human_male", "rp_human_male", "rp_human_male", "rp_human_male",
+		"rp_bothan_male", "rp_bothan_female", 
+		"rp_rodian_male", "rp_rodian_female", 
+		"rp_trandoshan_male", "rp_trandoshan_female",
+		"rp_twilek_male", "rp_twilek_female", 
+		"rp_zabrak_male", "rp_zabrak_female", 
+	},
+
+	skillTemplates = {
+			"bountyhunter"
+	},
+	
+	
+	equipmentTemplates = {
+		{"rp_human_male", {"mando_rebel"}},
+		{"rp_human_female", {"mando_rebel"}},
+		{"rp_rodian_male", {"mando_rebel"}},
+		{"rp_rodian_female", {"mando_rebel"}},
+		{"rp_trandoshan_male", {"mando_rebel"}},
+		{"rp_trandoshan_female", {"mando_rebel"}},
+		{"rp_twilek_male", {"mando_rebel"}},
+		{"rp_twilek_female", {"mando_rebel"}},
+		{"rp_zabrak_male", {"mando_rebel"}},
+		{"rp_zabrak_female", {"mando_rebel"}},
+	},	
+	
+	customizationTemplates = {
+		{"rp_human_male", {"random"}},
+		{"rp_human_female", {"random"}},
+		{"rp_rodian_male", {"random"}},
+		{"rp_rodian_female", {"random"}},
+		{"rp_trandoshan_male", {"random"}},
+		{"rp_trandoshan_female", {"random"}},
+		{"rp_twilek_male", {"random"}},
+		{"rp_twilek_female", {"random"}},
+		{"rp_zabrak_male", {"random"}},
+		{"rp_zabrak_female", {"random"}},
+	},
+
+	--armor = "stormtrooper",
+}
