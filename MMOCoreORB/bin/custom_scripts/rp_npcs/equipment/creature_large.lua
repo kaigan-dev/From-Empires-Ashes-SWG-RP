d06be310065148050132bfafd1de4f1461ef9fea
cagnaith <vthehc@yahoo.com>
2026-07-02 16:36:41 -0500

    feat: Remove armor from creature & vehicle tempolates.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/creature_large.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/creature_large.lua
@@ -1,5 +1,7 @@
 equipment = {
 	{"object/weapon/melee/unarmed/shared_unarmed_creature_large.iff", },
+	
+	--[[ No longer needed after the implementation of armor as an NPC "skill"
 	{"object/tangible/wearables/armor/roleplay/tantel/armor_tantel_skreej_helmet.iff", },
 	{"object/tangible/wearables/armor/roleplay/tantel/armor_tantel_skreej_chest_plate.iff", },
 	{"object/tangible/wearables/armor/roleplay/tantel/armor_tantel_skreej_bicep_r.iff", },
@@ -9,4 +11,5 @@ equipment = {
 	{"object/tangible/wearables/armor/roleplay/tantel/armor_tantel_skreej_gloves.iff", },
 	{"object/tangible/wearables/armor/roleplay/tantel/armor_tantel_skreej_bracer_l.iff", },
 	{"object/tangible/wearables/armor/roleplay/tantel/armor_tantel_skreej_bracer_r.iff", },
+	--]]
 }
--- MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/superbattledroid.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/superbattledroid.lua
@@ -1,28 +1,3 @@
 equipment = {
 	{"object/weapon/melee/unarmed/shared_unarmed_sbd.iff", },
-
-	-- 4/4 Armor
-    --[[
-	{"object/tangible/wearables/armor/roleplay/ithorian_guardian/ith_armor_s02_bicep_l.iff", "private/index_color_1", 204,},
-    {"object/tangible/wearables/armor/roleplay/ithorian_guardian/ith_armor_s02_bicep_r.iff", "private/index_color_1", 204,},
-    {"object/tangible/wearables/armor/roleplay/ithorian_guardian/ith_armor_s02_bracer_l.iff", "private/index_color_1", 204,},
-    {"object/tangible/wearables/armor/roleplay/ithorian_guardian/ith_armor_s02_bracer_r.iff", "private/index_color_1", 204,},
-    {"object/tangible/wearables/armor/roleplay/ithorian_guardian/ith_armor_s02_chest_plate.iff", "private/index_color_1", 155,},
-    {"object/tangible/wearables/armor/roleplay/ithorian_guardian/ith_armor_s02_gloves.iff", "private/index_color_1", 204,},
-    {"object/tangible/wearables/armor/roleplay/ithorian_guardian/ith_armor_s02_helmet.iff", "private/index_color_1", 213,},
-    {"object/tangible/wearables/armor/roleplay/ithorian_guardian/ith_armor_s02_leggings.iff", "private/index_color_1", 204,},
-    {"object/tangible/wearables/armor/roleplay/ithorian_guardian/ith_armor_s02_boots.iff", "private/index_color_1", 204,},
-    --]]
-    -- 6/6 Armor
-    --[[
-    {"object/tangible/wearables/armor/roleplay/ithorian_defender/ith_armor_s01_bicep_l.iff", "private/index_color_1", 204,},
-    {"object/tangible/wearables/armor/roleplay/ithorian_defender/ith_armor_s01_bicep_r.iff", "private/index_color_1", 204,},
-    {"object/tangible/wearables/armor/roleplay/ithorian_defender/ith_armor_s01_bracer_l.iff", "private/index_color_1", 204,},
-    {"object/tangible/wearables/armor/roleplay/ithorian_defender/ith_armor_s01_bracer_r.iff", "private/index_color_1", 204,},
-    {"object/tangible/wearables/armor/roleplay/ithorian_defender/ith_armor_s01_chest_plate.iff", "private/index_color_1", 155,},
-    {"object/tangible/wearables/armor/roleplay/ithorian_defender/ith_armor_s01_gloves.iff", "private/index_color_1", 204,},
-    {"object/tangible/wearables/armor/roleplay/ithorian_defender/ith_armor_s01_helmet.iff", "private/index_color_1", 213,},
-    {"object/tangible/wearables/armor/roleplay/ithorian_defender/ith_armor_s01_leggings.iff", "private/index_color_1", 204,},
-    {"object/tangible/wearables/armor/roleplay/ithorian_defender/ith_armor_s01_boots.iff", "private/index_color_1", 204,},
-    --]]
 }
