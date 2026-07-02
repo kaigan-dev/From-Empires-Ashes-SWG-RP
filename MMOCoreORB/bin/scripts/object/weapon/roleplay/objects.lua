1b39a4b4e4b7ac86d37a0bbcfbc44d7f42274ee4
cagnaith <vthehc@yahoo.com>
2026-07-02 14:53:13 -0500

    fix: Re-enable ion grenade. Move heavy weapons to the end of the weapons loading process.

--- MMOCoreORB/bin/scripts/object/weapon/roleplay/objects.lua
+++ MMOCoreORB/bin/scripts/object/weapon/roleplay/objects.lua
@@ -5,9 +5,11 @@ includeFile("weapon/roleplay/melee/unarmed/objects.lua")
 includeFile("weapon/roleplay/ranged/carbine/objects.lua")
 includeFile("weapon/roleplay/ranged/pistol/objects.lua")
 includeFile("weapon/roleplay/ranged/rifle/objects.lua")
-includefile("weapon/roleplay/ranged/heavy/objects.lua")
+
 includeFile("weapon/roleplay/thrown/grenade/objects.lua")
 
 includeFile("weapon/roleplay/lightsaber/single/1h/objects.lua")
 includeFile("weapon/roleplay/lightsaber/single/2h/objects.lua")
 includeFile("weapon/roleplay/lightsaber/double/objects.lua")
+
+includefile("weapon/roleplay/ranged/heavy/objects.lua")
