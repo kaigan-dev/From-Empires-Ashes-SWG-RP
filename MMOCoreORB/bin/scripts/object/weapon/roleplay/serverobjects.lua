307ff3c1b572fadd143e13bff507a52b19a423a7
canablyss <the.mac.atk@live.co.uk>
2026-07-04 14:48:33 +0100

    feat: serverobjects update

--- MMOCoreORB/bin/scripts/object/weapon/roleplay/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/weapon/roleplay/serverobjects.lua
@@ -5,10 +5,10 @@ includeFile("weapon/roleplay/melee/unarmed/serverobjects.lua")
 includeFile("weapon/roleplay/ranged/carbine/serverobjects.lua")
 includeFile("weapon/roleplay/ranged/pistol/serverobjects.lua")
 includeFile("weapon/roleplay/ranged/rifle/serverobjects.lua")
+includefile("weapon/roleplay/ranged/rifle/serverobjects.lua")
 includeFile("weapon/roleplay/thrown/grenade/serverobjects.lua")
 
 includeFile("weapon/roleplay/lightsaber/single/1h/serverobjects.lua")
 includeFile("weapon/roleplay/lightsaber/single/2h/serverobjects.lua")
 includeFile("weapon/roleplay/lightsaber/double/serverobjects.lua")
-
 includefile("weapon/roleplay/ranged/heavy/serverobjects.lua")
