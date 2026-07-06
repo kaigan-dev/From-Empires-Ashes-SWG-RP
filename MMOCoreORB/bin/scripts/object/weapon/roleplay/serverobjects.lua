d94864e34d574370eb1d73b2de7bf5cd547769c7
Ashlynne187 <Perfectrogue@gmail.com>
2026-07-06 16:19:39 -0500

    feat: Fixed includefile to be includeFile across everything

--- MMOCoreORB/bin/scripts/object/weapon/roleplay/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/weapon/roleplay/serverobjects.lua
@@ -5,7 +5,6 @@ includeFile("weapon/roleplay/melee/unarmed/serverobjects.lua")
 includeFile("weapon/roleplay/ranged/carbine/serverobjects.lua")
 includeFile("weapon/roleplay/ranged/pistol/serverobjects.lua")
 includeFile("weapon/roleplay/ranged/rifle/serverobjects.lua")
---includefile("weapon/roleplay/ranged/rifle/serverobjects.lua")   --Why are there two of these?
 includeFile("weapon/roleplay/thrown/grenade/serverobjects.lua")
 
 includeFile("weapon/roleplay/lightsaber/single/1h/serverobjects.lua")
