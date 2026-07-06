a51606cca6fe30ff842f5303704bb6b0cbe90408
cagnaith <vthehc@yahoo.com>
2026-07-06 13:39:25 -0500

    feat: Re-add flamethrower weapon template.

---
+++ MMOCoreORB/bin/scripts/object/weapon/roleplay/ranged/heavy/objects.lua
@@ -0,0 +1,5 @@
+object_weapon_roleplay_ranged_heavy_flamethrower = SharedWeaponObjectTemplate:new {
+	clientTemplateFileName = "object/weapon/roleplay/ranged/heavy/flamethrower.iff"
+}
+ObjectTemplates:addClientTemplate(object_weapon_roleplay_ranged_heavy_flamethrower, "object/weapon/roleplay/ranged/heavy/flamethrower.iff")
+--********************************************************************************************************************************************************
