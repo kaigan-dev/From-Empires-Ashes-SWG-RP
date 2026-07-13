008dfc30e468a87935451751d52049637ba8088b
Rev <47072690+Revanxx@users.noreply.github.com>
2026-07-14 00:04:12 +0200

    dh17 + a280cfe rifle fix 2

--- MMOCoreORB/bin/scripts/object/custom_content/weapon/ranged/objects.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/weapon/ranged/objects.lua
@@ -1206,7 +1206,7 @@ object_weapon_ranged_heavy_shared_ep3_loot_void = SharedWeaponObjectTemplate:new
 ObjectTemplates:addClientTemplate(object_weapon_ranged_heavy_shared_ep3_loot_void, "object/weapon/ranged/heavy/shared_ep3_loot_void.iff")
 --****************************************************************************************************************************************************************************
 object_weapon_ranged_rifle_shared_rifle_a280cfe = SharedWeaponObjectTemplate:new {
-	clientTemplateFileName = "object/weapon/ranged/rifle/a280cfe_blaster_rifle.iff"
+	clientTemplateFileName = "object/weapon/ranged/rifle/shared_rifle_a280cfe.iff"
 
 }
 
