d5cb2de809c3cc923daa0dca305d41bc0300a466
cagnaith <vthehc@yahoo.com>
2026-07-13 07:57:21 -0500

    feat: Add Send Ship Away to ship caller menu.

--- MMOCoreORB/bin/scripts/screenplays/borrie/rp_items/rp_shipCallerMenuComponent.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/rp_items/rp_shipCallerMenuComponent.lua
@@ -8,8 +8,18 @@ function RpShipCallerMenuComponent:fillObjectMenuResponse(pSceneObject, pMenuRes
 	if (pGhost == nil) then
 		return
 	end
+		
+	--menuResponse:addRadialMenuItem(20, 3, "Call Ship")
 
-	menuResponse:addRadialMenuItem(20, 3, "Call Ship")
+	local pShip = getShipFromControlDevice(pSceneObject)
+	local shipID = SceneObject(pShip):getObjectID()
+	local eventID = readData(shipID .. ":landShip:shipStatus")
+	if(eventID ~= 2 and eventID ~= 3) then
+		menuResponse:addRadialMenuItem(20, 3, "Land Ship")
+	elseif(eventID == 3) then
+		menuResponse:addRadialMenuItem(21, 3, "Send Ship Away")
+	end
+
 
 end
 
@@ -22,6 +32,12 @@ function RpShipCallerMenuComponent:handleObjectMenuSelect(pObject, pPlayer, sele
 		local pShip = getShipFromControlDevice(pObject)
 		local shipID = SceneObject(pShip):getObjectID()
 		BorRpShip:landShip(pShip, pPlayer)
+	end
+
+	if (selectedID == 21) then 
+		local pShip = getShipFromControlDevice(pObject)
+		local shipID = SceneObject(pShip):getObjectID()
+		BorRpShip:takeOffShip(pObject, pPlayer, true)
 	end
 
 	return 0
