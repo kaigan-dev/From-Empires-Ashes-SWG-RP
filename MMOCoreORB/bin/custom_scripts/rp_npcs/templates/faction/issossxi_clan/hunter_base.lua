4da50cfbe7fbef862ddb5b15df078a118aef76ed
Ashlynne187 <Perfectrogue@gmail.com>
2026-07-13 11:01:29 -0500

    Feat: add Issossxi clan base npcs

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/faction/issossxi_clan/hunter_base.lua
@@ -0,0 +1,52 @@
+npc_template = {
+
+	randomName = "human",
+	title = "a hunter",
+	baseTemplates = {
+		"rp_human_male", "rp_human_female", "rp_human_male", "rp_human_female","rp_human_male","rp_human_male",
+		"rp_rodian_male", "rp_rodian_female","rp_rodian_male", "rp_rodian_female",
+		"rp_trandoshan_male", "rp_trandoshan_female",
+		"rp_twilek_male", "rp_twilek_female",
+		"rp_zabrak_male", "rp_zabrak_female","rp_zabrak_male", "rp_zabrak_female",
+		"rp_nikto_male", "rp_nikto_male", "rp_nikto_male", "rp_nikto_male",
+		"rp_weequay_male","rp_weequay_male","rp_weequay_male",
+		"rp_nautolan_male",
+	},
+
+	skillTemplates = {"issossxi_clan/hunter_base"},
+	
+	
+	equipmentTemplates = {
+		{"rp_human_male", {"faction/issossxi_clan/hunter_base"}},
+		{"rp_human_female", {"faction/issossxi_clan/hunter_base"}},
+		{"rp_rodian_male", {"faction/issossxi_clan/hunter_base"}},
+		{"rp_rodian_female", {"faction/issossxi_clan/hunter_base"}},
+		{"rp_trandoshan_male", {"faction/issossxi_clan/hunter_base"}},
+		{"rp_trandoshan_female", {"faction/issossxi_clan/hunter_base"}},
+		{"rp_twilek_male", {"faction/issossxi_clan/hunter_base"}},
+		{"rp_twilek_female", {"faction/issossxi_clan/hunter_base"}},
+		{"rp_zabrak_male", {"faction/issossxi_clan/hunter_base"}},
+		{"rp_zabrak_female", {"faction/issossxi_clan/hunter_base"}},
+		{"rp_weequay_male", {"faction/issossxi_clan/hunter_base"}},
+		{"rp_nautolan_male", {"faction/issossxi_clan/hunter_base"}},
+		
+	},	
+	
+	customizationTemplates = {
+		{"rp_human_male", {"random"}},
+		{"rp_human_female", {"random"}},
+		{"rp_rodian_male", {"random"}},
+		{"rp_rodian_female", {"random"}},
+		{"rp_trandoshan_male", {"random"}},
+		{"rp_trandoshan_female", {"random"}},
+		{"rp_twilek_male", {"random"}},
+		{"rp_twilek_female", {"random"}},
+		{"rp_zabrak_male", {"random"}},
+		{"rp_zabrak_female", {"random"}},
+		{"rp_weequay_male", {"random"}},
+		{"rp_nautolan_male", {"random"}},
+		
+	},
+
+	--armor = "stormtrooper",
+}
