10b0d2d701b598702cbc6609993ea43c5a7310dd
Ashlynne187 <Perfectrogue@gmail.com>
2026-07-13 13:10:29 -0500

    added hunter equipment templates

--- MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/faction/issossxi_clan/hunter_base_armored.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/faction/issossxi_clan/hunter_base_armored.lua
@@ -1,9 +1,11 @@
 equipment = {
         {"object/weapon/melee/unarmed/unarmed_default_player.iff", "/private/index_color_blade", 0, },
-        {"object/tangible/wearables/boots/boots_s04.iff", "/private/index_color_1", 11, },
-        {"object/tangible/wearables/jacket/jacket_s21.iff", "/private/index_color_2", 62, "/private/index_color_1", 63, },
         {"object/tangible/wearables/belt/npe_belt_03.iff", },
         {"object/tangible/wearables/bandolier/bandolier_s04.iff", },
         {"object/tangible/wearables/pants/pants_s30.iff", "/private/index_color_1", 41, },
         {"object/tangible/wearables/shirt/shirt_s14.iff", "/private/index_color_2", 61, "/private/index_color_1", 118, },
+        {"object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_chest_plate.iff", },
+        {"object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_bracer_r.iff", },
+        {"object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_bracer_l.iff", },
+        {"object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_boots.iff", },
 }
---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/faction/issossxi_clan/hunter_base_armored3.lua
@@ -0,0 +1,12 @@
+equipment = {
+        {"object/weapon/melee/unarmed/unarmed_default_player.iff", "/private/index_color_blade", 0, },
+        {"object/tangible/wearables/belt/npe_belt_03.iff", },
+        {"object/tangible/wearables/bandolier/bandolier_s04.iff", },
+        {"object/tangible/wearables/shirt/shirt_s14.iff", "/private/index_color_2", 61, "/private/index_color_1", 118, },
+        {"object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_chest_plate.iff", },
+        {"object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_boots.iff", },
+        {"object/tangible/wearables/pants/pants_s25.iff", },
+        {"object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_gloves.iff", },
+        {"object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_bracer_r.iff", },
+        {"object/tangible/wearables/armor/roleplay/bone/armor_bone_s01_bicep_r.iff", },
+}
