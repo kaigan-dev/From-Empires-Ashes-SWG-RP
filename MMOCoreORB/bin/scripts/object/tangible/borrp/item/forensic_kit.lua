209f9fc717227b496aa17ce7a2b691c5c5ba6568
canablyss <the.mac.atk@live.co.uk>
2026-07-14 13:57:16 +0100

    feat: forensic kit

---
+++ MMOCoreORB/bin/scripts/object/tangible/borrp/item/forensic_kit.lua
@@ -0,0 +1,4 @@
+object_tangible_borrp_item_forensic_kit = object_tangible_borrp_item_shared_forensic_kit:new {
+
+}
+ObjectTemplates:addTemplate(object_tangible_borrp_item_forensic_kit, "object/tangible/borrp/item/forensic_kit.iff")
