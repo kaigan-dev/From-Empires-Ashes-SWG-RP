0e78e384deef7f50b2884a17ea60e429c3496390
Zapstrika <gravitybooster2@gmail.com>
2026-07-16 01:43:08 +0200

    Fixed spelling error in hutt trooper npc template

--- MMOCoreORB/bin/custom_scripts/rp_npcs/templates/faction/hutt/trooper_swamp.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/faction/hutt/trooper_swamp.lua
@@ -1,7 +1,7 @@
 npc_template = {
 
 	randomName = "human",
-	title = "a crminal footsoldier",
+	title = "a criminal footsoldier",
 	baseTemplates = {
 		"rp_human_male", "rp_human_female", "rp_human_male", "rp_human_female","rp_human_male","rp_human_male",
 		"rp_rodian_male", "rp_rodian_female","rp_rodian_male", "rp_rodian_female",
