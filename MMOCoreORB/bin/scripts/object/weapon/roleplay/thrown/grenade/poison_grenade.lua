2bd4a1751934ed80c2391815c80e40df96366c48
cagnaith <vthehc@yahoo.com>
2026-07-21 09:26:36 -0500

    feat: Fix grenade range.

--- MMOCoreORB/bin/scripts/object/weapon/roleplay/thrown/grenade/poison_grenade.lua
+++ MMOCoreORB/bin/scripts/object/weapon/roleplay/thrown/grenade/poison_grenade.lua
@@ -29,9 +29,8 @@ object_weapon_roleplay_thrown_grenade_poison_grenade = object_weapon_ranged_gren
 	maxRange = 48,
 	maxRangeAccuracy = 99,
 	
-	--area = 8,
-	area = 500,
-	
+	area = 8,
+		
 	dodgeDC = 16,
 	
 	rpSkillLevel = 6,
