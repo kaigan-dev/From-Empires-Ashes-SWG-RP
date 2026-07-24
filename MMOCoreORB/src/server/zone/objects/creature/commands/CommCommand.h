a81359766d2fe7cfb13adfa2fdce6a7ccdd1ea67
cagnaith <vthehc@yahoo.com>
2026-07-24 07:19:37 -0500

    fix: Include a target parameter in CommCommand's doQueueCommand even though we aren't doing anything with it.

--- MMOCoreORB/src/server/zone/objects/creature/commands/CommCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/CommCommand.h
@@ -23,7 +23,7 @@ public:
 
 	}
 
-	int doQueueCommand(CreatureObject* creature, const UnicodeString& arguments) const {
+	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const {
 
 		if (!checkStateMask(creature))
 			return INVALIDSTATE;
