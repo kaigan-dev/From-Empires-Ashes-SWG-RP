bea620a2ad115beedd775e296ced1d2cb985277b
Ashlynne187 <Perfectrogue@gmail.com>
2026-07-24 21:14:48 -0500

    feat: add bolotaur to npc templates

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/creatures/bolotaur.lua
@@ -0,0 +1,21 @@
+npc_template = {
+
+	name = "A Wampa",
+	baseTemplates = {
+		"bolotaur",
+	},
+
+	skillTemplates = {
+			"creatures/large"
+	},
+	
+	
+	equipmentTemplates = {
+		{"bolotaur", {"creature_large"}},
+	},	
+	
+	customizationTemplates = {
+	},
+
+	--armor = "stormtrooper",
+}
