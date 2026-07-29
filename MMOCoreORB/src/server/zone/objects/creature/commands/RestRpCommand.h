49a6878956d660684414c548fe93c41d41d463d1
cagnaith <vthehc@yahoo.com>
2026-07-29 09:17:39 -0500

    fix: Include header rather than cpp and hope that we're imagining the right ones.

--- MMOCoreORB/src/server/zone/objects/creature/commands/RestRpCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/RestRpCommand.h
@@ -7,7 +7,8 @@
 
 #include "server/zone/borrie/BorCharacter.h"
 #include "server/zone/objects/creature/sui/RestRPCommandSuiCallback.h"
-#include "server/zone/objects/region/CityRegionImplementation.cpp"
+#include "server/zone/objects/region/CityRegionImplementation.h"
+#include "server/zone/objects/region/CityRegion.h"
 
 class RestRpCommand : public QueueCommand {
 	
