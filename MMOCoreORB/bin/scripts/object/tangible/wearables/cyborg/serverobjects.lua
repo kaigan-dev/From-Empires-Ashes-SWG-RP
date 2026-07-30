af23236d3598ffd66ced4c73ce157904f25659f7
cagnaith <vthehc@yahoo.com>
2026-07-30 08:03:32 -0500

    fix: Remove cybernetics from loading in the hopes of stopping crashes.

--- MMOCoreORB/bin/scripts/object/tangible/wearables/cyborg/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/cyborg/serverobjects.lua
@@ -45,9 +45,13 @@
 --Children folder includes
 
 -- Server Objects
+
+--[[  These crash the server when loaded and upon character deletion. Do not use them without significant testing.
 includeFile("tangible/wearables/cyborg/arm_l_s01.lua")
 includeFile("tangible/wearables/cyborg/arm_r_s01.lua")
 includeFile("tangible/wearables/cyborg/forearm_l_s01.lua")
 includeFile("tangible/wearables/cyborg/forearm_r_s01.lua")
 includeFile("tangible/wearables/cyborg/hand_l_s01.lua")
 includeFile("tangible/wearables/cyborg/hand_r_s01.lua")
+
+--]]
