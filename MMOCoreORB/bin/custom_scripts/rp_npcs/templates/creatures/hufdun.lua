bb677757d5cac701c737a70d7dd2c0464a098066
Ashlynne187 <Perfectrogue@gmail.com>
2026-08-01 22:35:35 -0500

    feat: Added Huf-Duns to creature NPC templates

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/creatures/hufdun.lua
@@ -0,0 +1,22 @@
+npc_template = {
+
+	name = "A Huf-Dun",
+	baseTemplates = {
+		"huf_dun_hue",
+
+	},
+
+	skillTemplates = {
+			"creatures/large"
+	},
+	
+	
+	equipmentTemplates = {
+		{"huf_dun_hue", {"creature_large"}},
+	},	
+	
+	customizationTemplates = {
+	},
+
+	--armor = "stormtrooper",
+}
