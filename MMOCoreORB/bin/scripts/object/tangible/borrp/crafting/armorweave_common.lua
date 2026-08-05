10654d1208c4b1635decf6105fc0d2d2432e99ce
canablyss <the.mac.atk@live.co.uk>
2026-08-05 13:41:23 +0100

    feat:  crafting progress

--- MMOCoreORB/bin/scripts/object/tangible/borrp/crafting/armorweave_common.lua
+++ MMOCoreORB/bin/scripts/object/tangible/borrp/crafting/armorweave_common.lua
@@ -1,4 +1,7 @@
 object_tangible_borrp_crafting_armorweave_common = object_tangible_borrp_crafting_shared_armorweave:new {
 
+    customname = "Armorweave (Common)", 
+
+    rarity = "Common", 
 }
 ObjectTemplates:addTemplate(object_tangible_borrp_crafting_armorweave_common, "object/tangible/borrp/crafting/armorweave_common.iff")
