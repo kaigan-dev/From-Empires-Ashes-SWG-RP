dcc7e0e8282fa6c2c16ce9624ee9969c1b253361
cagnaith <vthehc@yahoo.com>
2026-08-15 08:47:00 -0500

    feat: Update dice roller output, part 2.

--- MMOCoreORB/src/server/zone/borrie/BorDice.h
+++ MMOCoreORB/src/server/zone/borrie/BorDice.h
@@ -89,11 +89,16 @@ public:
 				}
 				*/
 
-				if (mod != 0)
+				if (mod != 0) {
 						//return "Roll " + sNumDice + "d" + sDiceValue + ": " + DiceRollString + " (Modifier: " + String::valueOf(mod) + ") Result: " + String::valueOf(nResult + mod);
-						return "Roll " + sNumDice + "d" + sDiceValue + " " + String::valueOf(mod) + ": " + DiceRollString + "Result = " + String::valueOf(nResult + mod);
+					if(mod > 0) {
+						return "Roll " + sNumDice + "d" + sDiceValue + " +" + String::valueOf(mod) + ": " + DiceRollString + " +" + String::valueOf(mod) + " Result = " + String::valueOf(nResult + mod);
+					}
 					else
-						return "Roll " + sNumDice + "d" + sDiceValue + ": " + DiceRollString + " Result: " + String::valueOf(nResult);
+						return "Roll " + sNumDice + "d" + sDiceValue + String::valueOf(mod) + ": " + DiceRollString + " +" + String::valueOf(mod) + " Result = " + String::valueOf(nResult + mod);
+				}
+				else
+					return "Roll " + sNumDice + "d" + sDiceValue + ": " + DiceRollString + " Result: " + String::valueOf(nResult);
 
 			} else {
 				// Return, we need the full thing.
