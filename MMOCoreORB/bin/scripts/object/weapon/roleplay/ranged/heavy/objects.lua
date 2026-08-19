bdb8706d99d84fdc9cf3b8ca3816871be96f6262
canablyss <the.mac.atk@live.co.uk>
2026-08-19 10:09:21 +0100

    feat: test heavy

--- MMOCoreORB/bin/scripts/object/weapon/roleplay/ranged/heavy/objects.lua
+++ MMOCoreORB/bin/scripts/object/weapon/roleplay/ranged/heavy/objects.lua
@@ -3,3 +3,8 @@ object_weapon_roleplay_ranged_heavy_flamethrower = SharedWeaponObjectTemplate:ne
 }
 ObjectTemplates:addClientTemplate(object_weapon_roleplay_ranged_heavy_flamethrower, "object/weapon/roleplay/ranged/heavy/flamethrower.iff")
 --******************************************************************************************************************************************
+object_weapon_roleplay_ranged_heavy_heavy_acid_beam = SharedWeaponObjectTemplate:new {
+	clientTemplateFileName = "object/weapon/roleplay/ranged/heavy/heavy_acid_beam.iff"
+}
+ObjectTemplates:addClientTemplate(object_weapon_roleplay_ranged_heavy_heavy_acid_beam, "object/weapon/roleplay/ranged/heavy/heavy_acid_beam.iff")
+--******************************************************************************************************************************************
--- MMOCoreORB/bin/scripts/object/weapon/roleplay/ranged/heavy/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/weapon/roleplay/ranged/heavy/serverobjects.lua
@@ -1,1 +1,2 @@
 includeFile("weapon/roleplay/ranged/heavy/flamethrower.lua")
+includeFile("weapon/roleplay/ranged/heavy/heavy_acid_beam.lua")
