7529304f2453d49055cc90f8b0b67380c22e5287
cagnaith <vthehc@yahoo.com>
2026-08-31 18:11:05 -0500

    feat: Add Rebel SpecForce & Imperial Coruscant Trooper templates.

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/faction/imperial/trooper_coruscant.lua
@@ -0,0 +1,11 @@
+equipment = {
+        {"object/weapon/melee/unarmed/unarmed_default_player.iff", "/private/index_color_blade", 0, },
+        {"object/tangible/wearables/armor/roleplay/stormtrooper/armor_stormtrooper_helmet.iff", "/private/index_color_3", 10, },
+        {"object/tangible/wearables/armor/roleplay/stormtrooper/armor_stormtrooper_bicep_l.iff", "/private/index_color_3", 10, },
+        {"object/tangible/wearables/armor/roleplay/stormtrooper/armor_stormtrooper_bracer_l.iff", "/private/index_color_3", 10, },
+        {"object/tangible/wearables/armor/roleplay/stormtrooper/armor_stormtrooper_bracer_r.iff", "/private/index_color_3", 10, },
+        {"object/tangible/wearables/armor/roleplay/stormtrooper/armor_stormtrooper_bicep_r.iff", "/private/index_color_3", 10, },
+        {"object/tangible/wearables/armor/roleplay/stormtrooper/armor_stormtrooper_leggings.iff", },
+        {"object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_boots.iff", "/private/index_color_3", 10, },
+        {"object/tangible/wearables/armor/roleplay/stormtrooper/armor_stormtrooper_chest_plate.iff", },
+}
---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/faction/rebel/specforce.lua
@@ -0,0 +1,49 @@
+npc_template = {
+
+	randomName = "human",
+	baseTemplates = {"rp_human_male", "rp_human_female","rp_human_male", "rp_human_female",
+	"rp_bothan_male", "rp_bothan_female",
+	"rp_moncal_male", "rp_moncal_female",
+	"rp_sullustan_male", "rp_sullustan_female",
+	"rp_twilek_male", "rp_twilek_female",
+	"rp_zabrak_male", "rp_zabrak_female",
+},
+	skillTemplates = {"factional/common/elite"},
+	
+	equipmentTemplates = {
+		{"rp_human_male", {"faction/rebel/elite"}},
+		{"rp_human_female", {"faction/rebel/elite"}},
+		{"rp_bothan_male", {"faction/rebel/elite"}},
+		{"rp_bothan_female", {"faction/rebel/elite"}},
+		{"rp_moncal_male", {"faction/rebel/elite"}},
+		{"rp_moncal_female", {"faction/rebel/elite"}},
+		{"rp_sullustan_male", {"faction/rebel/elite"}},
+		{"rp_sullustan_female", {"faction/rebel/elite"}},
+		{"rp_twilek_male", {"faction/rebel/elite"}},
+		{"rp_twilek_female", {"faction/rebel/elite"}},
+		{"rp_zabrak_male", {"faction/rebel/elite"}},
+		{"rp_zabrak_female", {"faction/rebel/elite"}},
+
+	},	
+	
+	customizationTemplates = {
+		{"rp_human_male", {"random"}},
+		{"rp_human_female", {"random"}},
+		{"rp_bothan_male", {"random"}},
+		{"rp_bothan_female", {"random"}},
+		{"rp_moncal_male", {"random"}},
+		{"rp_moncal_female", {"random"}},
+		{"rp_sullustan_male", {"random"}},
+		{"rp_sullustan_female", {"random"}},
+		{"rp_twilek_male", {"random"}},
+		{"rp_twilek_female", {"random"}},
+		{"rp_zabrak_male", {"random"}},
+		{"rp_zabrak_female", {"random"}},
+	},	
+	
+	customVarOverrides = {
+		{"/shared_owner/blend_skinny", 71},
+		{"/shared_owner/blend_muscle", 0},
+		{"/shared_owner/blend_fat", 0},
+	}
+}
