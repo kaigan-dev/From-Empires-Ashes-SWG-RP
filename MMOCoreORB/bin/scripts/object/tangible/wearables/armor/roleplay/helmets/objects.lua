94a81867e8a29fa71f45dd0510359fe4990bfc87
NightShadowDark <noah8ward@gmail.com>
2026-09-04 09:49:48 -0500

    Added: Imp Ace Armored Helmet

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/roleplay/helmets/objects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/roleplay/helmets/objects.lua
@@ -14,3 +14,8 @@ object_tangible_wearables_armor_roleplay_helmets_shared_armor_blacksun_ace_helme
 }
 ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_roleplay_helmets_shared_armor_blacksun_ace_helmet, "object/tangible/wearables/armor/roleplay/helmets/shared_armor_blacksun_ace_helmet.iff")
 --********************************************************************************************************************************************************************************************************************
+object_tangible_wearables_armor_roleplay_helmets_shared_armor_imperial_ace_helmet = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/roleplay/helmets/shared_armor_imperial_ace_helmet.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_roleplay_helmets_shared_armor_imperial_ace_helmet, "object/tangible/wearables/armor/roleplay/helmets/shared_armor_imperial_ace_helmet.iff")
+--********************************************************************************************************************************************************************************************************************
--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/roleplay/helmets/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/roleplay/helmets/serverobjects.lua
@@ -1,3 +1,4 @@
 includeFile("tangible/wearables/armor/roleplay/helmets/armor_privateer_ace_helmet.lua")
 includeFile("tangible/wearables/armor/roleplay/helmets/armor_helmet_atat.lua")
 includeFile("tangible/wearables/armor/roleplay/helmets/armor_blacksun_ace_helmet.lua")
+includeFile("tangible/wearables/armor/roleplay/helmets/armor_imperial_ace_helmet.lua")
