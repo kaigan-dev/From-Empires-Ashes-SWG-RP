7c9d84da9c484c0f6f34f00a46b763b4dd601bf9
cagnaith <vthehc@yahoo.com>
2026-09-04 11:02:34 -0500

    feat: Add skill to 3PO for testing.

--- MMOCoreORB/bin/scripts/object/tangible/deed/pet_deed/deed_3p0_basic.lua
+++ MMOCoreORB/bin/scripts/object/tangible/deed/pet_deed/deed_3p0_basic.lua
@@ -59,6 +59,8 @@ object_tangible_deed_pet_deed_deed_3p0_basic = object_tangible_deed_pet_deed_sha
 	experimentalMax = {0, 0, 15, 0, 0, 100, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	experimentalPrecision = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	isMountable = 0,
+	skillTemplate = "droid.lua",
 }
 
 ObjectTemplates:addTemplate(object_tangible_deed_pet_deed_deed_3p0_basic, "object/tangible/deed/pet_deed/deed_3p0_basic.iff")
