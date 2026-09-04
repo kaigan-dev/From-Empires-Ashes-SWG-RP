8afe2f1890e2951c620d89ce079e8d1db1391295
NightShadowDark <noah8ward@gmail.com>
2026-09-04 09:16:02 -0500

    Add: Armored Black Sun Helmet

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/roleplay/helmets/objects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/roleplay/helmets/objects.lua
@@ -9,3 +9,8 @@ object_tangible_wearables_armor_roleplay_helmets_shared_armor_helmet_atat = Shar
 }
 ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_roleplay_helmets_shared_armor_helmet_atat, "object/tangible/wearables/armor/roleplay/helmets/shared_armor_helmet_atat.iff")
 --********************************************************************************************************************************************************************************************************************
+object_tangible_wearables_armor_roleplay_helmets_shared_armor_blacksun_ace_helmet = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/roleplay/helmets/shared_armor_blacksun_ace_helmet.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_roleplay_helmets_shared_armor_blacksun_ace_helmet, "object/tangible/wearables/armor/roleplay/helmets/shared_armor_blacksun_ace_helmet.iff")
+--********************************************************************************************************************************************************************************************************************
--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/roleplay/helmets/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/roleplay/helmets/serverobjects.lua
@@ -1,2 +1,3 @@
 includeFile("tangible/wearables/armor/roleplay/helmets/armor_privateer_ace_helmet.lua")
 includeFile("tangible/wearables/armor/roleplay/helmets/armor_helmet_atat.lua")
+includeFile("tangible/wearables/armor/roleplay/helmets/armor_blacksun_ace_helmet.lua")
