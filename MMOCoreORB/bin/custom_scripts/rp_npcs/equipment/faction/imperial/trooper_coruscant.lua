afeb58f54173a289678ce5ce2d37cf4d56c9e80e
cagnaith <vthehc@yahoo.com>
2026-09-06 08:41:09 -0500

    feat: Change coruscant trooper boots to use the RP item.

--- MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/faction/imperial/trooper_coruscant.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/faction/imperial/trooper_coruscant.lua
@@ -6,7 +6,7 @@ equipment = {
         {"object/tangible/wearables/armor/roleplay/stormtrooper/armor_stormtrooper_bracer_r.iff", "/private/index_color_3", 10, },
         {"object/tangible/wearables/armor/roleplay/stormtrooper/armor_stormtrooper_bicep_r.iff", "/private/index_color_3", 10, },
         {"object/tangible/wearables/armor/roleplay/stormtrooper/armor_stormtrooper_leggings.iff", },
-        {"object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_boots.iff", "/private/index_color_3", 10, },
+        {"object/tangible/wearables/armor/roleplay/stormtrooper/armor_stormtrooper_boots.iff", "/private/index_color_3", 10, },
         {"object/tangible/wearables/armor/roleplay/stormtrooper/armor_stormtrooper_chest_plate.iff", },
         {"object/tangible/wearables/armor/roleplay/stormtrooper/armor_stormtrooper_gloves.iff", },
 }
