4a6444a2590f9679a00967e3f3397c0e28b36e10
cagnaith <vthehc@yahoo.com>
2026-09-07 09:41:19 -0500

    feat: Reverse skill & equipment template names, as they were flipped.

--- MMOCoreORB/bin/scripts/object/tangible/deed/pet_deed/cu_pa_deed.lua
+++ MMOCoreORB/bin/scripts/object/tangible/deed/pet_deed/cu_pa_deed.lua
@@ -58,8 +58,8 @@ object_tangible_deed_pet_deed_cu_pa_deed = object_tangible_deed_pet_deed_shared_
 	controlDeviceObjectTemplate = "object/intangible/pet/cu_pa_hue.iff",
 	mobileTemplate = "cu_pa_be",
 	isMountable = 1,
-	skillTemplate = "creature_medium",
-	equipmentTemplate = "creatures/medium",
+	skillTemplate = "creatures/medium",
+	equipmentTemplate = "creature_medium",
 }
 
 ObjectTemplates:addTemplate(object_tangible_deed_pet_deed_cu_pa_deed, "object/tangible/deed/pet_deed/cu_pa_deed.iff")
