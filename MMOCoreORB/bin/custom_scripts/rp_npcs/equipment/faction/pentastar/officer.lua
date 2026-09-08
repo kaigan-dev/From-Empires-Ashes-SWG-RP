8cdd3cd3bedd23ddd2a3dc72352c0ce0d0f0ff66
Serenissima697 <serenissima.republica@gmail.com>
2026-09-08 17:12:46 +0100

    feat: trying again

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/faction/pentastar/officer.lua
@@ -0,0 +1,6 @@
+equipment = {
+        {"object/weapon/melee/unarmed/unarmed_default_player.iff", "/private/index_color_blade", 0, },
+        {"object/tangible/wearables/pants/pants_s25.iff", "/private/index_color_2", 6, "/private/index_color_1", 242, },
+        {"object/tangible/tcg/series1/wearable_naboo_jacket.iff", "/private/index_color_2", 6, "/private/index_color_1", 242, },
+        {"object/tangible/wearables/boots/boots_s15.iff", "/private/index_color_1", 117, },
+}
---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/faction/pentastar/trooper.lua
@@ -0,0 +1,39 @@
+npc_template = {
+
+	randomName = "zabrak",
+	baseTemplates = {"rp_human_male", "rp_human_female", "rp_rodian_male", "rp_rodian_female", "rp_zabrak_male", "rp_zabrak_female", "rp_trandoshan_male", "rp_trandoshan_female", "rp_aqualish_male", "rp_aqualish_female"},
+	skillTemplates = {"factional/common/trooper"},
+	
+	equipmentTemplates = {
+		{"rp_human_male", {"faction/pentastar/trooper"}},
+		{"rp_human_female", {"faction/pentastar/trooper"}},
+		{"rp_rodian_male", {"faction/pentastar/trooper"}},
+		{"rp_rodian_female", {"faction/pentastar/trooper"}},
+		{"rp_zabrak_male", {"faction/pentastar/trooper"}},
+		{"rp_zabrak_female", {"faction/pentastar/trooper"}},
+		{"rp_trandoshan_male", {"faction/pentastar/trooper"}},
+		{"rp_trandoshan_female", {"faction/pentastar/trooper"}},
+		{"rp_aqualish_male", {"faction/pentastar/trooper"}},
+		{"rp_aqualish_female", {"faction/pentastar/trooper"}},
+	},	
+	
+	customizationTemplates = {
+		{"rp_human_male", {"random"}},
+		{"rp_human_female", {"random"}},
+		{"rp_rodian_male" , {"random"}},
+		{"rp_rodian_female" , {"random"}},
+		{"rp_zabrak_male" , {"random"}},
+		{"rp_zabrak_female" , {"random"}},
+		{"rp_trandoshan_male" , {"random"}},
+		{"rp_trandoshan_female" , {"random"}},
+		{"rp_aqualish_male" , {"random"}},
+		{"rp_aqualish_female" , {"random"}},
+	},	
+	
+	customVarOverrides = {
+		{"/shared_owner/blend_skinny", 71},
+		{"/shared_owner/blend_muscle", 0},
+		{"/shared_owner/blend_fat", 0},
+		{"height", 1.2},
+	}
+}
