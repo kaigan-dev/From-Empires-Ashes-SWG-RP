ffb53a25e19461a7cd0ee807f37547602e5b6aab
Serenissima697 <serenissima.republica@gmail.com>
2026-09-08 17:28:05 +0100

    feat: trying again, again

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/faction/rebel/dantooine_trooper.lua
@@ -0,0 +1,11 @@
+equipment = {
+        {"object/weapon/melee/unarmed/unarmed_default_player.iff", "/private/index_color_blade", 0, },
+        {"object/tangible/wearables/belt/npe_belt_01.iff", "/private/index_color_1", 37, },
+        {"object/tangible/wearables/backpack/backpack_s01.iff", },
+        {"object/tangible/wearables/shirt/shirt_s09.iff", "/private/index_color_2", 181, "/private/index_color_1", 176, },
+        {"object/tangible/wearables/vest/vest_hutt_gang_s01.iff", },
+        {"object/tangible/wearables/pants/pants_s21.iff", "/private/index_color_2", 92, "/private/index_color_1", 177, },
+        {"object/tangible/wearables/boots/boots_s05.iff", "/private/index_color_2", 91, "/private/index_color_1", 23, },
+        {"object/tangible/wearables/gloves/gloves_s02.iff", "/private/index_color_1", 89, },
+        {"object/tangible/wearables/helmet/helmet_s06.iff", "/private/index_color_2", 223, "/private/index_color_1", 218, },
+}
