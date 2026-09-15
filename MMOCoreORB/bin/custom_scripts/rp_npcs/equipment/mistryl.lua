b30f82facd714f23a2bc408cbee101cd7248e4a5
cagnaith <vthehc@yahoo.com>
2026-09-15 17:13:22 -0500

    feat: Add mistryl equipment template.

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/mistryl.lua
@@ -0,0 +1,10 @@
+equipment = {
+        {"object/weapon/melee/unarmed/unarmed_default_player.iff", "/private/index_color_blade", 0, },
+        {"object/tangible/wearables/hood/hood_robe_s01.iff", "/private/index_color_1", 6, },
+        {"object/tangible/wearables/gloves/gloves_s13.iff", "/private/index_color_1", 26, },
+        {"object/tangible/wearables/bodysuit/bodysuit_s16.iff", "/private/index_color_1", 6, },
+        {"object/tangible/wearables/armor/ubese/armor_ubese_bandolier.iff", },
+        {"object/tangible/wearables/belt/npe_belt_01.iff", "/private/index_color_1", 37, },
+        {"object/tangible/wearables/boots/boots_s15.iff", "/private/index_color_2", 96, "/private/index_color_1", 28, },
+        {"object/tangible/wearables/goggles/goggles_s01.iff", "/private/index_color_2", 16, "/private/index_color_1", 126, },
+}
