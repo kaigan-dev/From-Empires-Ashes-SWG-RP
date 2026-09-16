102eb3afe521dd5ee3a436f0aac2c60dfc70e412
cagnaith <vthehc@yahoo.com>
2026-09-15 20:55:33 -0500

    feat: Add Dantooine mounted police equipment template.

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/planet/dantooine/mountedpolice.lua
@@ -0,0 +1,12 @@
+equipment = {
+        {"object/weapon/melee/unarmed/unarmed_default_player.iff", "/private/index_color_blade", 0, },
+        {"object/tangible/wearables/jacket/jacket_s10.iff", "/private/index_color_1", 13, },
+        {"object/tangible/wearables/pants/pants_s28.iff", "/private/index_color_2", 31, "/private/index_color_1", 13, },
+        {"object/tangible/wearables/boots/boots_s21.iff", "/private/index_color_2", 117, "/private/index_color_1", 31, },
+        {"object/tangible/wearables/gloves/gloves_s06.iff", "/private/index_color_1", 11, },
+        {"object/tangible/wearables/belt/npe_belt_02.iff", },
+        {"object/tangible/wearables/hat/hat_s04.iff", "/private/index_color_2", 55, "/private/index_color_1", 13, },
+        {"object/tangible/wearables/goggles/goggles_s06.iff", "/private/index_color_1", 0, "/private/index_color_0", 83, },
+        {"object/tangible/wearables/shirt/shirt_s13.iff", "/private/index_color_1", 14, },
+        {"object/tangible/wearables/backpack/backpack_s01.iff", },
+}
