3f7153118b45dd71568528620d93bfe2b6ec522d
canablyss <the.mac.atk@live.co.uk>
2026-09-18 16:38:33 +0100

    feat: woops

--- MMOCoreORB/bin/scripts/screenplays/borrie/spawnmaps/dantooine_khoonda.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/spawnmaps/dantooine_khoonda.lua
@@ -461,8 +461,9 @@ function DantooineKhoondaScreenPlay:spawnMobiles()
     -- Pebblefizz staff
     pNpc = spawnRoleplayMobile("rp_dantooine", "rp_base_npc", 1, 0.2, 2, 14.1, 184, 610039731, "dressed_commoner_fat_human_female_01", "default", "default", "default") --Equip, Skill, Customization
 	setRandomCreatureName(pNpc, "Receptionist Jeniper")
-    pNpc = spawnRoleplayMobile("rp_dantooine", "rp_base_npc", 1, 16.9, 2, 0.2, 162, 610039733, "loveday_protocol_droid", "default", "default", "default") --Equip, Skill, Customization
+    pNpc = spawnRoleplayMobile("rp_dantooine", "rp_base_npc", 1, 16.1, 2, -1, 175, 610039733, "loveday_protocol_droid", "default", "default", "default") --Equip, Skill, Customization
 	setRandomCreatureName(pNpc, "Yes Man")
+    
 
 
 
