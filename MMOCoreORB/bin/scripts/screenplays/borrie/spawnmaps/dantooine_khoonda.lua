89145c67f4886c443343f0424dfe45bcd6b23003
cagnaith <vthehc@yahoo.com>
2026-09-18 11:46:01 -0500

    feat: Fix names on Pebblefizz NPCs.

--- MMOCoreORB/bin/scripts/screenplays/borrie/spawnmaps/dantooine_khoonda.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/spawnmaps/dantooine_khoonda.lua
@@ -460,9 +460,9 @@ function DantooineKhoondaScreenPlay:spawnMobiles()
 
     -- Pebblefizz staff
     pNpc = spawnRoleplayMobile("rp_dantooine", "rp_base_npc", 1, 0.2, 2, 14.1, 184, 610039731, "dressed_commoner_fat_human_female_01", "default", "default", "default") --Equip, Skill, Customization
-	setRandomCreatureName(pNpc, "Receptionist Jeniper")
+	SceneObject(pNpc):setCustomObjectName("Receptionist Jeniper")
     pNpc = spawnRoleplayMobile("rp_dantooine", "rp_base_npc", 1, 16.1, 2, -1, 175, 610039733, "loveday_protocol_droid", "default", "default", "default") --Equip, Skill, Customization
-	setRandomCreatureName(pNpc, "Yes Man")
+	SceneObject(pNpc):setCustomObjectName("Yes Man")
     
 
 
