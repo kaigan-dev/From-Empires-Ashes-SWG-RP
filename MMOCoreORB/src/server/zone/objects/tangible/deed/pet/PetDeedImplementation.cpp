0d00883c924b6d9ee11ca97f59ce7c3144920fa4
cagnaith <vthehc@yahoo.com>
2026-09-22 10:02:28 -0500

    feat: Add another pause before the call when creating pets.

--- MMOCoreORB/src/server/zone/objects/tangible/deed/pet/PetDeedImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/deed/pet/PetDeedImplementation.cpp
@@ -548,7 +548,7 @@ int PetDeedImplementation::handleObjectMenuSelect(CreatureObject* player, byte s
 			destroyObjectFromWorld(true);
 		}
 
-		//std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(2));
+		std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(2));
 		controlDevice->callObject(player);
 
 		generated = true;
