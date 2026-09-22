88e8fc0e63ec3f840ad7a7961eb23f96e3b966d6
cagnaith <vthehc@yahoo.com>
2026-09-22 09:08:08 -0500

    fix: More syntax corrections.

--- MMOCoreORB/src/server/zone/objects/tangible/deed/pet/PetDeedImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/deed/pet/PetDeedImplementation.cpp
@@ -546,7 +546,7 @@ int PetDeedImplementation::handleObjectMenuSelect(CreatureObject* player, byte s
 			destroyObjectFromWorld(true);
 		}
 
-		sleep_until(std::chrono::system_clock::now() + chrono::seconds(2));
+		sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(2));
 		controlDevice->callObject(player);
 
 		generated = true;
