eb7c1c3f7d820d56510a01544c428cdb3f0ad5bc
cagnaith <vthehc@yahoo.com>
2026-09-25 05:49:39 -0500

    feat: Add first of many new equipment templates.

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/themed/biker1.lua
@@ -0,0 +1,8 @@
+equipment = {
+	{"object/weapon/melee/unarmed/unarmed_default_player.iff", "/private/index_color_blade", 0, },
+	{"object/tangible/wearables/pants/pants_s25.iff", "/private/index_color_2", 6, "/private/index_color_1", 6, },
+	{"object/tangible/wearables/shirt/shirt_s38.iff", "/private/index_color_2", 59, "/private/index_color_1", 59, },
+	{"object/tangible/wearables/jacket/jacket_s08.iff", "/private/index_color_2", 132, "/private/index_color_1", 6, },
+	{"object/tangible/wearables/boots/boots_s14.iff", "/private/index_color_2", 182, "/private/index_color_1", 31, },
+	{"object/tangible/wearables/goggles/goggles_s03.iff", "/private/index_color_1", 2, },
+}
